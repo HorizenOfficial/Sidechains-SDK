dea8ed5230e9aaf53e0e65506c63c8f4af787735
Oleksandr Iozhytsia <Oleksandr.Iozhytsia@infopulse.com>
2019-01-17 16:21:06 +0200

    Fix errors after merge

--- src/main/java/com/horizen/transaction/RegularTransactionSerializer.java
+++ src/main/java/com/horizen/transaction/RegularTransactionSerializer.java
@@ -1,27 +1,9 @@
 package com.horizen.transaction;
 
-import com.google.common.primitives.Bytes;
-import com.google.common.primitives.Ints;
-import com.google.common.primitives.Longs;
-import com.horizen.box.RegularBox;
-import com.horizen.box.RegularBoxSerializer;
-import com.horizen.proof.ProofOfKnowledge;
-import com.horizen.proposition.PublicKey25519Proposition;
-import com.horizen.proposition.PublicKey25519PropositionSerializer;
-import com.horizen.proposition.Signature25519;
-import com.horizen.proposition.Signature25519Serializer;
-import javafx.util.Pair;
-import scala.util.Success;
-import scala.util.Failure;
 import scala.util.Try;
-import scorex.core.serialization.Serializer;
 
-import java.io.ByteArrayOutputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 
-class RegularTransactionSerializer implements TransactionSerializer<RegularTransaction>
+public class RegularTransactionSerializer implements TransactionSerializer<RegularTransaction>
 {
 
     private static RegularTransactionSerializer serializer;
