67aa1b79a6f15c29a7042c2bf2849a7247b29f28
Oleksandr Iozhytsia <Oleksandr.Iozhytsia@infopulse.com>
2019-02-01 15:11:06 +0200

    RegularTransaction serialization regression test added.

--- src/test/java/com/horizen/transaction/RegularTransactionSerializerTest.java
+++ src/test/java/com/horizen/transaction/RegularTransactionSerializerTest.java
@@ -9,6 +9,8 @@ import org.junit.Before;
 import org.junit.Test;
 import scala.util.Try;
 
+import java.io.File;
+import java.nio.file.Files;
 import java.util.ArrayList;
 
 import static org.junit.Assert.*;
@@ -39,6 +41,7 @@ public class RegularTransactionSerializerTest {
         to.add(new Pair<>(pk5.publicImage(), 20L));
         to.add(new Pair<>(pk6.publicImage(), 90L));
 
+        // Note: current transaction bytes are also stored in "src/test/resources/regular_transaction_bytes"
         transaction = RegularTransaction.create(from, to, fee, timestamp);
     }
 
@@ -53,6 +56,26 @@ public class RegularTransactionSerializerTest {
 
         boolean failureExpected = serializer.parseBytes("broken bytes".getBytes()).isFailure();
         assertEquals("Failure during parsing expected", true, failureExpected);
+    }
 
+    @Test
+    public void RegularTransactionSerializerTest_RegressionTest() {
+        byte[] bytes;
+        try {
+            bytes = Files.readAllBytes(new File("src\\test\\resources\\regular_transaction_bytes").toPath());
+        }
+        catch (Exception e) {
+            assertEquals(e.toString(), true, false);
+            return;
+        }
+
+        TransactionSerializer serializer = transaction.serializer();
+        Try<RegularTransaction> t = serializer.parseBytes(bytes);
+        assertEquals("Transaction serialization failed.", true, t.isSuccess());
+
+        RegularTransaction parsedTransaction = t.get();
+        assertEquals("Transaction is different to origin.", transaction.id(), parsedTransaction.id());
+        assertEquals("Transaction is different to origin.", transaction.fee(), parsedTransaction.fee());
+        assertEquals("Transaction is different to origin.", transaction.timestamp(), parsedTransaction.timestamp());
     }
 }
