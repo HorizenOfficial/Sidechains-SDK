f87820d3139851ab99bcb5ed4668674612f80e7a
g666 <giuseppe.fusco.666@gmail.com>
2019-09-09 17:33:37 +0200

    Added route to SidechainNodeApiRoute. They are equal to scorex PeersApiRoute but in future in our sdk will be replaced circe json library.

--- src/main/scala/com/horizen/api/http/SidechainNodeApiRoute.scala
+++ src/main/scala/com/horizen/api/http/SidechainNodeApiRoute.scala
@@ -10,17 +10,48 @@ import scorex.core.settings.RESTApiSettings
 
 import scala.concurrent.ExecutionContext
 import scala.util.{Failure, Success}
+import io.circe.Json
 import io.circe.generic.auto._
-import scorex.core.network.NetworkController.ReceivableMessages.ConnectTo
+import io.circe.syntax._
+import scorex.core.api.http.PeersApiRoute.{BlacklistedPeers, PeerInfoResponse}
+import scorex.core.network.NetworkController.ReceivableMessages.{ConnectTo, GetConnectedPeers}
 import scorex.core.network.peer.PeerInfo
+import scorex.core.network.peer.PeerManager.ReceivableMessages.{GetAllPeers, GetBlacklistedPeers}
+import scorex.core.utils.NetworkTimeProvider
 
-case class SidechainNodeApiRoute(networkController: ActorRef, override val settings: RESTApiSettings, sidechainNodeViewHolderRef: ActorRef)
+case class SidechainNodeApiRoute(peerManager: ActorRef,
+                                 networkController: ActorRef,
+                                 timeProvider: NetworkTimeProvider,
+                                 override val settings: RESTApiSettings, sidechainNodeViewHolderRef: ActorRef)
                                 (implicit val context: ActorRefFactory, override val ec : ExecutionContext) extends SidechainApiRoute {
 
   override val route : Route = (pathPrefix("node"))
-            {connect ~ getAllPeersInfo}
+            {connect ~ allPeers ~ connectedPeers ~ blacklistedPeers}
 
   private val addressAndPortRegexp = "([\\w\\.]+):(\\d{1,5})".r
+
+  def allPeers: Route = (path("all") & get) {
+    val result = askActor[Map[InetSocketAddress, PeerInfo]](peerManager, GetAllPeers).map {
+      _.map { case (address, peerInfo) =>
+        PeerInfoResponse.fromAddressAndInfo(address, peerInfo)
+      }
+    }
+    ApiResponse(result)
+  }
+
+  def connectedPeers: Route = (path("connected") & get) {
+    val result = askActor[Seq[PeerInfo]](networkController, GetConnectedPeers).map {
+      _.map { peerInfo =>
+        PeerInfoResponse(
+          address = peerInfo.peerSpec.address.map(_.toString).getOrElse(""),
+          lastSeen = peerInfo.lastSeen,
+          name = peerInfo.peerSpec.nodeName,
+          connectionType = peerInfo.connectionType.map(_.toString)
+        )
+      }
+    }
+    ApiResponse(result)
+  }
 
   def connect : Route = (post & path("connect"))
   {
@@ -44,9 +75,10 @@ case class SidechainNodeApiRoute(networkController: ActorRef, override val setti
     }
   }
 
-  def getAllPeersInfo : Route = (post & path("getAllPeer sInfo"))
-  {
-    ApiResponse.OK
+  def blacklistedPeers: Route = (path("blacklisted") & get) {
+    val result = askActor[Seq[InetAddress]](peerManager, GetBlacklistedPeers)
+      .map(x => BlacklistedPeers(x.map(_.toString)).asJson)
+    ApiResponse(result)
   }
 
 }
