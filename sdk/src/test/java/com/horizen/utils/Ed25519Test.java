53da0836c69b5d710be07460078d253d8f96ec94
g666 <giuseppe.fusco.666@gmail.com>
2019-11-20 09:46:11 +0100

    Refactoring of the test suite according to the code review.

--- sdk/src/test/java/com/horizen/utils/Ed25519Test.java
+++ sdk/src/test/java/com/horizen/utils/Ed25519Test.java
@@ -10,87 +10,177 @@ import static org.junit.Assert.*;
 public class Ed25519Test {
 
     @Test
-    public void testKeiPair() {
+    public void deterministicKeysGeneration() {
         byte[] seed = "seed1".getBytes();
-        Pair<byte[], byte[]> kp1 = Ed25519.createKeyPair(seed);
-        Pair<byte[], byte[]> kp2 = Ed25519.createKeyPair(seed);
-        Pair<byte[], byte[]> kp3 = Ed25519.createKeyPair("seed2".getBytes());
-        byte[] pvk = kp1.getKey();
-        byte[] pvk2 = kp2.getKey();
-        assertTrue("Private keys are not the same.", Arrays.equals(pvk, pvk2));
-        assertFalse("Private keys are the same.", Arrays.equals(pvk, kp3.getKey()));
+
+        Pair<byte[], byte[]> keyPair1 = Ed25519.createKeyPair(seed);
+        Pair<byte[], byte[]> keyPair2 = Ed25519.createKeyPair(seed);
+
+        byte[] privateKey1 = keyPair1.getKey();
+        byte[] privateKey2 = keyPair2.getKey();
+        byte[] publicKey1 = keyPair1.getValue();
+        byte[] publicKey2 = keyPair2.getValue();
+
+        assertTrue("Private keys are not the same.", Arrays.equals(privateKey1, privateKey2));
+        assertTrue("Public keys are not the same.", Arrays.equals(publicKey1, publicKey2));
+    }
+
+    @Test
+    public void deterministicSignature() {
+        byte[] seed = "seed1".getBytes();
+
+        Pair<byte[], byte[]> keyPair1 = Ed25519.createKeyPair(seed);
+        Pair<byte[], byte[]> keyPair2 = Ed25519.createKeyPair(seed);
+
+        byte[] privateKey1 = keyPair1.getKey();
+        byte[] privateKey2 = keyPair2.getKey();
+        byte[] publicKey1 = keyPair1.getValue();
+        byte[] publicKey2 = keyPair2.getValue();
+
         byte[] msg = "message to sign".getBytes();
-        byte[] signature = Ed25519.sign(pvk, msg, kp1.getValue());
-        boolean isVerified = Ed25519.verify(signature, msg, kp1.getValue());
+        byte[] signature1 = Ed25519.sign(privateKey1, msg, publicKey1);
+        byte[] signature2 = Ed25519.sign(privateKey2, msg, publicKey2);
+
+        assertTrue("Signatures are not the same.", Arrays.equals(signature1, signature2));
+
+        byte[] differentPrivateKey = Arrays.copyOf(privateKey1, 32);
+        differentPrivateKey[0] = privateKey1[1];
+        byte[] signature3 = Ed25519.sign(differentPrivateKey, msg, publicKey1);
+        assertFalse("Signatures are the same.", Arrays.equals(signature1, signature3));
+    }
+
+    @Test
+    public void signatureVerification() {
+        byte[] seed = "seed1".getBytes();
+
+        Pair<byte[], byte[]> keyPair1 = Ed25519.createKeyPair(seed);
+        Pair<byte[], byte[]> keyPair2 = Ed25519.createKeyPair(seed);
+
+        byte[] privateKey1 = keyPair1.getKey();
+        byte[] publicKey1 = keyPair1.getValue();
+        byte[] publicKey2 = keyPair2.getValue();
+
+        byte[] msg = "message to sign".getBytes();
+        byte[] signature = Ed25519.sign(privateKey1, msg, publicKey1);
+
+        boolean isVerified = Ed25519.verify(signature, msg, publicKey1);
         assertTrue("Signature is not verified.", isVerified);
-        isVerified = Ed25519.verify(signature, msg, kp2.getValue());
+
+        isVerified = Ed25519.verify(signature, msg, publicKey2);
         assertTrue("Signature is not verified.", isVerified);
+
         String strBadPublicKey = "dc172b93ad5e563bf4932c70e1245034c35467ef2efd4d64ebf819683467e2bf";
         byte[] badPublicKey = Hex.decode(strBadPublicKey);
         isVerified = Ed25519.verify(signature, msg, badPublicKey);
         assertFalse("Signature is verified.", isVerified);
     }
 
+    /**
+     * https://tools.ietf.org/html/rfc8032#page-24
+     * Test Vectors for Ed25519
+     * -----TEST 1
+     * <p>
+     * Given a private key, a public key, the message signed and a signature,
+     * verify that the signing algorithm is deterministic.
+     */
     @Test
     public void rfc_8032_test_1() {
         String strPrivateKey = "9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60";
         String strPublicKey = "d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a";
         String strMsg = "";
         String strSignature = "e5564300c360ac729086e2cc806e828a84877f1eb8e5d974d873e065224901555fb8821590a33bacc61e39701cf9b46bd25bf5f0595bbe24655141438e7a100b";
+
         byte[] msg = Hex.decode(strMsg);
         byte[] privateKey = Hex.decode(strPrivateKey);
         byte[] publicKey = Hex.decode(strPublicKey);
         byte[] signature = Hex.decode(strSignature);
-        byte[] signatureBC = Ed25519.sign(privateKey, msg, publicKey);
-        boolean isVerified = Ed25519.verify(signatureBC, msg, publicKey);
-        assertTrue("Signatures are not the same.", Arrays.equals(signature, signatureBC));
+
+        byte[] ed25519SignatureBC = Ed25519.sign(privateKey, msg, publicKey);
+        boolean isVerified = Ed25519.verify(ed25519SignatureBC, msg, publicKey);
+
+        assertTrue("Signatures are not the same.", Arrays.equals(signature, ed25519SignatureBC));
         assertTrue("Signature is not verified.", isVerified);
+
         String strBadPublicKey = "dc172b93ad5e563bf4932c70e1245034c35467ef2efd4d64ebf819683467e2bf";
         byte[] badPublicKey = Hex.decode(strBadPublicKey);
+
         isVerified = Ed25519.verify(signature, msg, badPublicKey);
         assertFalse("Signature is verified.", isVerified);
     }
 
+    /**
+     * https://tools.ietf.org/html/rfc8032#page-24
+     * Test Vectors for Ed25519
+     * -----TEST 2
+     * <p>
+     * Given a private key, a public key, the message signed and a signature,
+     * verify that the signing algorithm is deterministic.
+     */
     @Test
     public void rfc_8032_test_2() {
         String strPrivateKey = "4ccd089b28ff96da9db6c346ec114e0f5b8a319f35aba624da8cf6ed4fb8a6fb";
         String strPublicKey = "3d4017c3e843895a92b70aa74d1b7ebc9c982ccf2ec4968cc0cd55f12af4660c";
         String strMsg = "72";
         String strSignature = "92a009a9f0d4cab8720e820b5f642540a2b27b5416503f8fb3762223ebdb69da085ac1e43e15996e458f3613d0f11d8c387b2eaeb4302aeeb00d291612bb0c00";
+
         byte[] msg = Hex.decode(strMsg);
         byte[] privateKey = Hex.decode(strPrivateKey);
         byte[] publicKey = Hex.decode(strPublicKey);
         byte[] signature = Hex.decode(strSignature);
-        byte[] signatureBC = Ed25519.sign(privateKey, msg, publicKey);
-        boolean isVerified = Ed25519.verify(signatureBC, msg, publicKey);
-        assertTrue("Signatures are not the same.", Arrays.equals(signature, signatureBC));
+
+        byte[] ed25519SignatureBC = Ed25519.sign(privateKey, msg, publicKey);
+        boolean isVerified = Ed25519.verify(ed25519SignatureBC, msg, publicKey);
+
+        assertTrue("Signatures are not the same.", Arrays.equals(signature, ed25519SignatureBC));
         assertTrue("Signature is not verified.", isVerified);
+
         String strBadPublicKey = "dc172b93ad5e563bf4932c70e1245034c35467ef2efd4d64ebf819683467e2bf";
         byte[] badPublicKey = Hex.decode(strBadPublicKey);
         isVerified = Ed25519.verify(signature, msg, badPublicKey);
         assertFalse("Signature is verified.", isVerified);
     }
 
+    /**
+     * https://tools.ietf.org/html/rfc8032#page-24
+     * Test Vectors for Ed25519
+     * -----TEST 3
+     * <p>
+     * Given a private key, a public key, the message signed and a signature,
+     * verify that the signing algorithm is deterministic.
+     */
     @Test
     public void rfc_8032_test_3() {
         String strPrivateKey = "c5aa8df43f9f837bedb7442f31dcb7b166d38535076f094b85ce3a2e0b4458f7";
         String strPublicKey = "fc51cd8e6218a1a38da47ed00230f0580816ed13ba3303ac5deb911548908025";
         String strMsg = "af82";
         String strSignature = "6291d657deec24024827e69c3abe01a30ce548a284743a445e3680d7db5ac3ac18ff9b538d16f290ae67f760984dc6594a7c15e9716ed28dc027beceea1ec40a";
+
         byte[] msg = Hex.decode(strMsg);
         byte[] privateKey = Hex.decode(strPrivateKey);
         byte[] publicKey = Hex.decode(strPublicKey);
         byte[] signature = Hex.decode(strSignature);
-        byte[] signatureBC = Ed25519.sign(privateKey, msg, publicKey);
-        boolean isVerified = Ed25519.verify(signatureBC, msg, publicKey);
-        assertTrue("Signatures are not the same.", Arrays.equals(signature, signatureBC));
+
+        byte[] ed25519SignatureBC = Ed25519.sign(privateKey, msg, publicKey);
+        boolean isVerified = Ed25519.verify(ed25519SignatureBC, msg, publicKey);
+
+        assertTrue("Signatures are not the same.", Arrays.equals(signature, ed25519SignatureBC));
         assertTrue("Signature is not verified.", isVerified);
+
         String strBadPublicKey = "dc172b93ad5e563bf4932c70e1245034c35467ef2efd4d64ebf819683467e2bf";
         byte[] badPublicKey = Hex.decode(strBadPublicKey);
+
         isVerified = Ed25519.verify(signature, msg, badPublicKey);
         assertFalse("Signature is verified.", isVerified);
     }
 
+    /**
+     * https://tools.ietf.org/html/rfc8032#page-24
+     * Test Vectors for Ed25519
+     * -----TEST 1024
+     * <p>
+     * Given a private key, a public key, the message signed and a signature,
+     * verify that the signing algorithm is deterministic.
+     */
     @Test
     public void rfc_8032_test_1024() {
         String strPrivateKey = "f5e5767cf153319517630f226876b86c8160cc583bc013744c6bf255f5cc0ee5";
@@ -115,36 +205,54 @@ public class Ed25519Test {
                 "faa933a15ef1369546868a7f3a45a96768d40fd9d03412c091c6315cf4fde7cb68606937380db2eaaa707b4c4185c32eddcdd306705e4d" +
                 "c1ffc872eeee475a64dfac86aba41c0618983f8741c5ef68d3a101e8a3b8cac60c905c15fc910840b94c00a0b9d0";
         String strSignature = "0aab4c900501b3e24d7cdf4663326a3a87df5e4843b2cbdb67cbf6e460fec350aa5371b1508f9f4528ecea23c436d94b5e8fcd4f681e30a6ac00a9704a188a03";
+
         byte[] msg = Hex.decode(strMsg);
         byte[] privateKey = Hex.decode(strPrivateKey);
         byte[] publicKey = Hex.decode(strPublicKey);
         byte[] signature = Hex.decode(strSignature);
-        byte[] signatureBC = Ed25519.sign(privateKey, msg, publicKey);
-        boolean isVerified = Ed25519.verify(signatureBC, msg, publicKey);
-        assertTrue("Signatures are not the same.", Arrays.equals(signature, signatureBC));
+
+        byte[] ed25519SignatureBC = Ed25519.sign(privateKey, msg, publicKey);
+        boolean isVerified = Ed25519.verify(ed25519SignatureBC, msg, publicKey);
+
+        assertTrue("Signatures are not the same.", Arrays.equals(signature, ed25519SignatureBC));
         assertTrue("Signature is not verified.", isVerified);
+
         String strBadPublicKey = "dc172b93ad5e563bf4932c70e1245034c35467ef2efd4d64ebf819683467e2bf";
         byte[] badPublicKey = Hex.decode(strBadPublicKey);
+
         isVerified = Ed25519.verify(signature, msg, badPublicKey);
         assertFalse("Signature is verified.", isVerified);
     }
 
+    /**
+     * https://tools.ietf.org/html/rfc8032#page-24
+     * Test Vectors for Ed25519
+     * -----TEST sha
+     * <p>
+     * Given a private key, a public key, the message signed and a signature,
+     * verify that the signing algorithm is deterministic.
+     */
     @Test
     public void rfc_8032_test_sha() {
         String strPrivateKey = "833fe62409237b9d62ec77587520911e9a759cec1d19755b7da901b96dca3d42";
         String strPublicKey = "ec172b93ad5e563bf4932c70e1245034c35467ef2efd4d64ebf819683467e2bf";
         String strMsg = "ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f";
         String strSignature = "dc2a4459e7369633a52b1bf277839a00201009a3efbf3ecb69bea2186c26b58909351fc9ac90b3ecfdfbc7c66431e0303dca179c138ac17ad9bef1177331a704";
+
         byte[] msg = Hex.decode(strMsg);
         byte[] privateKey = Hex.decode(strPrivateKey);
         byte[] publicKey = Hex.decode(strPublicKey);
         byte[] signature = Hex.decode(strSignature);
-        byte[] signatureBC = Ed25519.sign(privateKey, msg, publicKey);
-        boolean isVerified = Ed25519.verify(signatureBC, msg, publicKey);
-        assertTrue("Signatures are not the same.", Arrays.equals(signature, signatureBC));
+
+        byte[] ed25519SignatureBC = Ed25519.sign(privateKey, msg, publicKey);
+        boolean isVerified = Ed25519.verify(ed25519SignatureBC, msg, publicKey);
+
+        assertTrue("Signatures are not the same.", Arrays.equals(signature, ed25519SignatureBC));
         assertTrue("Signature is not verified.", isVerified);
+
         String strBadPublicKey = "dc172b93ad5e563bf4932c70e1245034c35467ef2efd4d64ebf819683467e2bf";
         byte[] badPublicKey = Hex.decode(strBadPublicKey);
+
         isVerified = Ed25519.verify(signature, msg, badPublicKey);
         assertFalse("Signature is verified.", isVerified);
 
