dbf19d95a6de43870d71dfee8a4f02cb3131426e
Oleg Aleksandrov <Oleg.Aleksandrov@infopulse.com>
2020-01-02 19:59:17 +0200

    Add Forging box serialization/VRF part changes

--- sdk/src/main/scala/com/horizen/vrf/VRFSecretKey.scala
+++ sdk/src/main/scala/com/horizen/vrf/VRFSecretKey.scala
@@ -1,7 +1,9 @@
 package com.horizen.vrf
 
-class VRFSecretKey {
+class VRFSecretKey(key: Array[Byte]) {
   def prove(message: Array[Byte]): VRFProof = ??? // jni call to Rust impl
 
   def vrfHash(message: Array[Byte]): Array[Byte] = ??? // if need // jni call to Rust impl
+
+  def bytes: Array[Byte] = ???
 }
