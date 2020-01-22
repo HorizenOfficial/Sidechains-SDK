29b5be76ac634104933b0bc128e08bd1eaadf35f
Oleg Aleksandrov <Oleg.Aleksandrov@infopulse.com>
2020-01-22 13:58:17 +0200

    Add TimeToSlotEpoch converter

--- sdk/src/main/scala/com/horizen/SidechainSettings.scala
+++ sdk/src/main/scala/com/horizen/SidechainSettings.scala
@@ -1,34 +1,33 @@
 package com.horizen
 
 import scorex.core.settings.ScorexSettings
+
 import scala.concurrent.duration.FiniteDuration
 
 
-case class WebSocketSettings(
-                        address: String,
-                        connectionTimeout: FiniteDuration,
-                        reconnectionDelay: FiniteDuration,
-                        reconnectionMaxAttempts: Int
+case class WebSocketSettings(address: String,
+                             connectionTimeout: FiniteDuration,
+                             reconnectionDelay: FiniteDuration,
+                             reconnectionMaxAttempts: Int
                             )
 
-case class GenesisDataSettings(
-                        scGenesisBlockHex: String,
-                        scId: String,
-                        mcBlockHeight: Int,
-                        powData: String,
-                        mcNetwork: String,
-                        withdrawalEpochLength: Int
+case class GenesisDataSettings(scGenesisBlockHex: String,
+                               scId: String,
+                               mcBlockHeight: Int,
+                               powData: String,
+                               mcNetwork: String,
+                               withdrawalEpochLength: Int,
+                               consensusSecondsInSlot: Int,
+                               consensusSlotsInEpoch: Int
                               )
 
-case class WalletSettings(
-                        seed: String,
-                        genesisSecrets: Seq[String]
+case class WalletSettings(seed: String,
+                          genesisSecrets: Seq[String]
                          )
 
 
-case class SidechainSettings(
-                        scorexSettings: ScorexSettings,
-                        genesisData: GenesisDataSettings,
-                        websocket: WebSocketSettings,
-                        wallet: WalletSettings
+case class SidechainSettings(scorexSettings: ScorexSettings,
+                             genesisData: GenesisDataSettings,
+                             websocket: WebSocketSettings,
+                             wallet: WalletSettings
                             )
--- sdk/src/main/scala/com/horizen/consensus/package.scala
+++ sdk/src/main/scala/com/horizen/consensus/package.scala
@@ -1,24 +1,42 @@
 package com.horizen
 
-import scorex.core.block.Block
+import java.math.BigInteger
+
+import scorex.util.ModifierId
 import supertagged.TaggedType
 
 package object consensus {
   val rootHashLen: Int = 32
-
-  case class StakeConsensusEpochInfo(rootHash: Array[Byte], totalStake: Long)
-  case class NonceConsensusEpochInfo(consensusNonce: ConsensusNonce)
-
-  case class FullConsensusEpochInfo(stakeConsensusEpochInfo: StakeConsensusEpochInfo, nonceConsensusEpochInfo: NonceConsensusEpochInfo)
+  val consensusHardcodedSaltString: String = "TEST"
+  val stakePercentPrecision: BigInteger = BigInteger.valueOf(10000) // where 1 / STAKE_PERCENT_PRECISION -- minimal possible stake percentage to be able to forge
 
   object ConsensusEpochNumber extends TaggedType[Int]
   type ConsensusEpochNumber = ConsensusEpochNumber.Type
   def intToConsensusEpochNumber(consensusEpochNumber: Int): ConsensusEpochNumber = ConsensusEpochNumber @@ consensusEpochNumber
+
+  object ConsensusEpochId extends TaggedType[String]
+  type ConsensusEpochId = ConsensusEpochId.Type
+  def epochIdFromBlockId(blockId: ModifierId): ConsensusEpochId = ConsensusEpochId @@ blockId
+
+  object ConsensusSlotNumber extends TaggedType[Int]
+  type ConsensusSlotNumber = ConsensusSlotNumber.Type
+  def intToConsensusSlotNumber(consensusSlotNumber: Int): ConsensusSlotNumber = ConsensusSlotNumber @@ consensusSlotNumber
 
   object ConsensusNonce extends TaggedType[Int]
   type ConsensusNonce = ConsensusNonce.Type
   def intToConsensusNonce(consensusNonce: Int): ConsensusNonce = ConsensusNonce @@ consensusNonce
 
 
-  def timeStampToEpochNumber(timestamp: Block.Timestamp): ConsensusEpochNumber = ???
+  def buildVrfMessage(slotNumber: ConsensusSlotNumber, nonce: ConsensusNonce): Array[Byte] = {
+    val slotNumberAsString = slotNumber.toString
+    val nonceAsString = nonce.toString
+
+    (nonceAsString + slotNumberAsString + consensusHardcodedSaltString).getBytes
+  }
+
+  def hashToStakePercent(bytes: Array[Byte]): Double = {
+    //@TODO check correctness!
+    val hashAsNumber = new BigInteger(1, bytes)
+    hashAsNumber.remainder(stakePercentPrecision).doubleValue()
+  }
 }
