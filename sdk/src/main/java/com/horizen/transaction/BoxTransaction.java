2c261f016560d5c9c39cc049c999dd7530ae4168
Oleksandr Iozhytsia <iojitsa@gmail.com>
2020-04-23 17:28:31 +0300

    Minor clean-up.

--- sdk/src/main/java/com/horizen/transaction/BoxTransaction.java
+++ sdk/src/main/java/com/horizen/transaction/BoxTransaction.java
@@ -76,8 +76,7 @@ public abstract class BoxTransaction<P extends Proposition, B extends Box<P>> ex
     public TransactionIncompatibilityChecker incompatibilityChecker() {
         return new DefaultTransactionIncompatibilityChecker();
     }
-
-    // TODO: maybe final as well?
+    
     @Override
     public byte[] messageToSign() {
         ByteArrayOutputStream unlockersStream = new ByteArrayOutputStream();
