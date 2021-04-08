0e0f7db19156e56b109c8c300da6732969d6d59e
i-Alex <iojitsa@gmail.com>
2021-04-08 20:11:01 +0300

    Transactions semanticValidity checks improved.

--- sdk/src/main/java/com/horizen/transaction/SidechainTransaction.java
+++ sdk/src/main/java/com/horizen/transaction/SidechainTransaction.java
@@ -3,10 +3,13 @@ package com.horizen.transaction;
 import com.google.common.primitives.Bytes;
 import com.google.common.primitives.Ints;
 import com.google.common.primitives.Longs;
+import com.horizen.box.CoinsBox;
 import com.horizen.box.NoncedBox;
 import com.horizen.box.BoxUnlocker;
+import com.horizen.box.WithdrawalRequestBox;
 import com.horizen.proposition.Proposition;
 import com.horizen.utils.BytesUtils;
+import com.horizen.utils.ZenCoinsUtils;
 import scorex.crypto.hash.Blake2b256;
 
 import java.io.ByteArrayOutputStream;
@@ -57,7 +60,13 @@ abstract public class SidechainTransaction<P extends Proposition, B extends Nonc
 
     public abstract boolean transactionSemanticValidity();
 
-    // We check, that nonces for new boxes were enforced according to our algorithm. Then do inheritors check.
+    // We check, that:
+    // 1) there is no double spend boxes.
+    // 2) nonces for new boxes were enforced according to our algorithm;
+    // 3) coin balances are valid;
+    // 4) non-coin boxes values are non-negative;
+    // 5) fee is non-negative.
+    // Then do inheritors check.
     @Override
     public final boolean semanticValidity() {
         // Check that transaction doesn't make a double spend,
@@ -65,16 +74,26 @@ abstract public class SidechainTransaction<P extends Proposition, B extends Nonc
         if(unlockers().size() != boxIdsToOpen().size())
             return false;
 
-        // Check output boxes nonce correctness and non-negative value.
+        // Check output boxes nonce correctness and coin box values.
+        long coinsCumulatedValue = 0;
         List<B> boxes = newBoxes();
         for(int i = 0; i < boxes.size(); i++) {
             B box = boxes.get(i);
             if(box.nonce() != getNewBoxNonce(box.proposition(), i)) {
                 return false;
             }
-            // check box values
-            if(box.value() < 0)
-                return false;
+            // check coins box value
+            if(box instanceof CoinsBox || box instanceof WithdrawalRequestBox) {
+                if(!ZenCoinsUtils.isValidMoneyRange(box.value()))
+                    return false;
+                coinsCumulatedValue += box.value();
+                if(!ZenCoinsUtils.isValidMoneyRange(coinsCumulatedValue))
+                    return false;
+            } else {
+                // Non-coins box should have at least non-negative value
+                if (box.value() < 0)
+                    return false;
+            }
         }
 
         if(fee() < 0)
