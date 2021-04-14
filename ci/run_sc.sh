249712df7250e80513575bf6b8a89eed27ebf8e7
cronicc <cronic@zensystem.io>
2021-04-14 17:49:27 +0000

    Update ci/run_sc.h
    * bail on error
    * test for regular folder/file on args
    * grep for correct arch of libjemalloc, library could be installed for multiple arches if cross compiling/emulating
    * don't overwrite $LD_PRELOAD but prepend to possibly existing values
    * use exec to start java
    * add SIMPLE_APP_VERISON var
    * quoting/globbing issues and formatting

--- ci/run_sc.sh
+++ ci/run_sc.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-if [ $1 ] && [ $2 ]
-then
-	path_to_jemalloc=$(ldconfig -p | grep libjemalloc.so | cut -d'>' -f 2 | head -n 1)
-	if [ $path_to_jemalloc ]
-	then
-		echo "Starting sidechain..."
-		LD_PRELOAD="$path_to_jemalloc" java -cp $1/target/sidechains-sdk-simpleapp-0.2.7.jar:$1/target/lib/* com.horizen.examples.SimpleApp $2
-	else
-		echo "Could not find jemalloc library. Please install jemalloc to keep memory consumption in check." 
-	fi
+
+set -eo pipefail
+
+SIMPLE_APP_VERSION="${SIMPLE_APP_VERSION:-0.2.7}"
+
+if [ -d "$1" ] && [ -f "$2" ]; then
+  path_to_jemalloc="$(ldconfig -p | grep "$(arch)" | grep 'libjemalloc\.so\.1$' | tr -d ' ' | cut -d '>' -f 2)"
+  if [ -f "$path_to_jemalloc" ]; then
+    echo "Starting sidechain..."
+    LD_PRELOAD="$path_to_jemalloc:$LD_PRELOAD" exec java -cp "${1}/target/sidechains-sdk-simpleapp-${SIMPLE_APP_VERSION}.jar:${1}/target/lib/*" com.horizen.examples.SimpleApp "$2"
+  else
+    echo "Could not find jemalloc library. Please install jemalloc to keep memory consumption in check."
+    exit 1
+  fi
 else
-	echo "Usage: runsh.sh <path_to_app> <path_to_config_file>"
+  echo "Usage: runsh.sh <path_to_app_dir> <path_to_config_file>"
+  exit 1
 fi
