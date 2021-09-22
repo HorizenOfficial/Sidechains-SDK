338d102da721e886ed3c05deb35985abc285fed7
Oleksandr Iozhytsia <iojitsa@gmail.com>
2021-09-22 15:56:02 +0300

    Horizen address json serialization fix.

--- sdk/src/main/scala/com/horizen/SidechainApp.scala
+++ sdk/src/main/scala/com/horizen/SidechainApp.scala
@@ -3,7 +3,6 @@ package com.horizen
 import java.lang.{Byte => JByte}
 import java.nio.file.{Files, Paths}
 import java.util.{HashMap => JHashMap, List => JList}
-
 import akka.actor.ActorRef
 import akka.http.scaladsl.server.{ExceptionHandler, RejectionHandler}
 import com.google.inject.name.Named
@@ -41,6 +40,7 @@ import scala.collection.immutable.Map
 import scala.collection.mutable
 import scala.io.{Codec, Source}
 import com.horizen.network.SidechainNodeViewSynchronizer
+import com.horizen.serialization.JsonHorizenPublicKeyHashSerializer
 
 import scala.util.Try
 
@@ -156,6 +156,9 @@ class SidechainApp @Inject()
     )
     case _ => throw new IllegalArgumentException("Configuration file scorex.genesis.mcNetwork parameter contains inconsistent value.")
   }
+
+  // Configure Horizen address json serializer specifying proper network type.
+  JsonHorizenPublicKeyHashSerializer.setNetworkType(params)
 
   // Generate Coboundary Marlin Proving System dlog keys
   log.info(s"Generating Coboundary Marlin Proving System dlog keys. It may take some time.")
