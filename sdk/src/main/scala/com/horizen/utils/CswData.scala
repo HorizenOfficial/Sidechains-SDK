d5ab2c9f82e37766a7eb7cfcd7432e66ec7347ea
Oleksandr Iozhytsia <iojitsa@gmail.com>
2021-11-23 17:38:24 +0200

    SidechainWalletCswDataStorage test added.

--- sdk/src/main/scala/com/horizen/utils/CswData.scala
+++ sdk/src/main/scala/com/horizen/utils/CswData.scala
@@ -4,6 +4,8 @@ import com.horizen.librustsidechains.FieldElement
 import scorex.core.serialization.{BytesSerializable, ScorexSerializer}
 import scorex.util.serialization.{Reader, Writer}
 
+import java.util
+
 
 sealed trait CswData extends BytesSerializable
 
@@ -16,9 +18,29 @@ case class UtxoCswData(boxId: Array[Byte],
   override type M = UtxoCswData
 
   override def serializer: ScorexSerializer[UtxoCswData] = UtxoCswDataSerializer
+
+  override def hashCode(): Int = {
+    var result = util.Arrays.hashCode(boxId)
+    result = 31 * result + util.Arrays.hashCode(spendingPubKey)
+    result = 31 * result + amount.hashCode()
+    result = 31 * result + nonce.hashCode()
+    result = 31 * result + util.Arrays.hashCode(customHash)
+    result = 31 * result + util.Arrays.hashCode(utxoMerklePath)
+    result
+  }
+
+  override def equals(obj: Any): Boolean = {
+    obj match {
+      case other: UtxoCswData => boxId.sameElements(other.boxId) && spendingPubKey.sameElements(other.spendingPubKey) &&
+        amount == other.amount && nonce == other.nonce && customHash.sameElements(other.customHash) &&
+        utxoMerklePath.sameElements(other.utxoMerklePath)
+      case _ => false
+    }
+  }
+
+  override def toString = s"UtxoCswData(boxID = ${BytesUtils.toHexString(boxId)})"
 }
 
-// TODO: add path to FT in CommitmentTree
 case class ForwardTransferCswData(boxId: Array[Byte],
                                   receivedPubKey: Array[Byte],
                                   mcReturnAddress: Array[Byte],
@@ -32,6 +54,33 @@ case class ForwardTransferCswData(boxId: Array[Byte],
   override type M = ForwardTransferCswData
 
   override def serializer: ScorexSerializer[ForwardTransferCswData] = ForwardTransferCswDataSerializer
+
+  override def hashCode(): Int = {
+    var result = util.Arrays.hashCode(boxId)
+    result = 31 * result + util.Arrays.hashCode(receivedPubKey)
+    result = 31 * result + util.Arrays.hashCode(mcReturnAddress)
+    result = 31 * result + util.Arrays.hashCode(mcTxHash)
+    result = 31 * result + outIdx
+    result = 31 * result + util.Arrays.hashCode(scCommitmentMerklePath)
+    result = 31 * result + util.Arrays.hashCode(btrCommitment)
+    result = 31 * result + util.Arrays.hashCode(certCommitment)
+    result = 31 * result + util.Arrays.hashCode(scCrCommitment)
+    result = 31 * result + util.Arrays.hashCode(ftMerklePath)
+    result
+  }
+
+  override def equals(obj: Any): Boolean = {
+    obj match {
+      case other: ForwardTransferCswData => boxId.sameElements(other.boxId) && receivedPubKey.sameElements(other.receivedPubKey) &&
+        mcReturnAddress.sameElements(other.mcReturnAddress) && mcTxHash.sameElements(other.mcTxHash) &&
+        outIdx == other.outIdx && scCommitmentMerklePath.sameElements(other.scCommitmentMerklePath) &&
+        btrCommitment.sameElements(other.btrCommitment) && certCommitment.sameElements(other.certCommitment) &&
+        scCrCommitment.sameElements(other.scCrCommitment) && ftMerklePath.sameElements(other.ftMerklePath)
+      case _ => false
+    }
+  }
+
+  override def toString = s"ForwardTransferCswData(boxID = ${BytesUtils.toHexString(boxId)})"
 }
 
 object CswDataSerializer extends ScorexSerializer[CswData] {
