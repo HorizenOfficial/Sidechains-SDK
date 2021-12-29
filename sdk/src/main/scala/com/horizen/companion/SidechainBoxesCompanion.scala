86fa1d6108deb4f85949ff754faecf63c6ffec5d
VolodymyrBabaiev <Volodymyr.Babaiev@infopulse.com>
2021-12-29 03:43:15 +0200

    Updated DynamicTypeSerializer.

--- sdk/src/main/scala/com/horizen/companion/SidechainBoxesCompanion.scala
+++ sdk/src/main/scala/com/horizen/companion/SidechainBoxesCompanion.scala
@@ -1,6 +1,5 @@
 package com.horizen.companion
 
-
 import java.util.{HashMap => JHashMap}
 import java.lang.{Byte => JByte}
 
@@ -10,12 +9,12 @@ import com.horizen.box._
 import com.horizen.utils.DynamicTypedSerializer
 
 
-case class SidechainBoxesCompanion(customSerializers: JHashMap[JByte, BoxSerializer[SidechainTypes#SCB]])
+case class SidechainBoxesCompanion(customBoxSerializers: JHashMap[JByte, BoxSerializer[SidechainTypes#SCB]])
   extends DynamicTypedSerializer[SidechainTypes#SCB, BoxSerializer[SidechainTypes#SCB]](
     new JHashMap[JByte, BoxSerializer[SidechainTypes#SCB]]() {{
         put(ZenBoxId.id(), ZenBoxSerializer.getSerializer.asInstanceOf[BoxSerializer[SidechainTypes#SCB]])
         put(WithdrawalRequestBoxId.id(), WithdrawalRequestBoxSerializer.getSerializer.asInstanceOf[BoxSerializer[SidechainTypes#SCB]])
         put(ForgerBoxId.id(), ForgerBoxSerializer.getSerializer.asInstanceOf[BoxSerializer[SidechainTypes#SCB]])
-      }},
-    customSerializers)
+    }},
+    customBoxSerializers)
 
