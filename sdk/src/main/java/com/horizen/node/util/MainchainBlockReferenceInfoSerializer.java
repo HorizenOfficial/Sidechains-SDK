2e5ecbd4b35c53b67940261d2f9fae6e8fbc18e7
VolodymyrBabaiev <Volodymyr.Babaiev@infopulse.com>
2021-12-29 03:19:56 +0200

    Updated MainchainReferenceInfo serialization.

--- sdk/src/main/java/com/horizen/node/util/MainchainBlockReferenceInfoSerializer.java
+++ sdk/src/main/java/com/horizen/node/util/MainchainBlockReferenceInfoSerializer.java
@@ -1,8 +1,10 @@
 package com.horizen.node.util;
 
+import com.horizen.CommonParams;
 import scorex.core.serialization.ScorexSerializer;
 import scorex.util.serialization.Reader;
 import scorex.util.serialization.Writer;
+
 
 public class MainchainBlockReferenceInfoSerializer implements ScorexSerializer<MainchainBlockReferenceInfo> {
     private static MainchainBlockReferenceInfoSerializer serializer;
@@ -21,11 +23,26 @@ public class MainchainBlockReferenceInfoSerializer implements ScorexSerializer<M
 
     @Override
     public void serialize(MainchainBlockReferenceInfo referenceInfo, Writer writer) {
-        writer.putBytes(referenceInfo.bytes());
+        writer.putBytes(referenceInfo.getMainchainHeaderHash());
+        writer.putBytes(referenceInfo.getParentMainchainHeaderHash());
+        writer.putInt(referenceInfo.getMainchainHeight());
+        writer.putBytes(referenceInfo.getMainchainHeaderSidechainBlockId());
+        writer.putBytes(referenceInfo.getMainchainReferenceDataSidechainBlockId());
     }
 
     @Override
     public MainchainBlockReferenceInfo parse(Reader reader) {
-        return MainchainBlockReferenceInfo.parseBytes(reader.getBytes(reader.remaining()));
+        byte[] mainchainBlockReferenceHash = reader.getBytes(CommonParams.mainchainBlockHashLength());
+        byte[] parentMainchainBlockReferenceHash = reader.getBytes(CommonParams.mainchainBlockHashLength());
+        int mainchainHeight = reader.getInt();
+        byte[] mainchainHeaderSidechainBlockId = reader.getBytes(CommonParams.sidechainIdLength());
+        byte[] mainchainReferenceDataSidechainBlockId = reader.getBytes(CommonParams.sidechainIdLength());
+
+        return new MainchainBlockReferenceInfo(mainchainBlockReferenceHash,
+                                               parentMainchainBlockReferenceHash,
+                                               mainchainHeight,
+                                               mainchainHeaderSidechainBlockId,
+                                               mainchainReferenceDataSidechainBlockId);
+
     }
 }
