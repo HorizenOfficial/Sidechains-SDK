fa16ba5b141c19ad9c224f4f8431495342571d3d
MarcoOl94 <marco.olivero94@libero.it>
2022-04-21 16:12:25 +0200

    [SCHAINS-431] Refactored openStakeTransaction related endpoints

--- qa/httpCalls/transaction/openStake.py
+++ qa/httpCalls/transaction/openStake.py
@@ -1,6 +1,6 @@
 import json
-#execute a transaction/openStake call
-def createOpenStakeTransaction(sidechainNode, boxid, address, forger_index, fee, format = False):
+#execute a transaction/createopenStakeTransaction call
+def createOpenStakeTransaction(sidechainNode, boxid, address, forger_index, fee, format = False, automaticSend = False):
       j = { 
             "transactionInput": 
             { 
@@ -9,8 +9,22 @@ def createOpenStakeTransaction(sidechainNode, boxid, address, forger_index, fee,
             "regularOutputProposition": address,
             "forgerListIndex": forger_index,
             "fee": fee,
-            "format": format 
+            "format": format,
+            "automaticSend": automaticSend
       }
       request = json.dumps(j)
       response = sidechainNode.transaction_createOpenStakeTransaction(request)
+      return response["result"]
+
+#execute a transaction/createOpenStakeTransactionSimplified call
+def createOpenStakeTransactionSimplified(sidechainNode, forger_proposition, forger_index, fee, format = False, automaticSend = False):
+      j = {
+            "forgerProposition": forger_proposition,
+            "forgerListIndex": forger_index,
+            "fee": fee,
+            "format": format,
+            "automaticSend": automaticSend
+      }
+      request = json.dumps(j)
+      response = sidechainNode.transaction_createOpenStakeTransactionSimplified(request)
       return response["result"]
