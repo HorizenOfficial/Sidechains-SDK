d9ddcd7349d7873ce46013a1fe485be8d624ff79
Oleksandr Iozhytsia <iojitsa@gmail.com>
2022-05-18 17:57:08 +0300

    AccountState initial commit

---
+++ sdk/src/main/scala/com/horizen/state/StateReader.scala
@@ -0,0 +1,20 @@
+package com.horizen.state
+
+import com.horizen.block.WithdrawalEpochCertificate
+import com.horizen.box.WithdrawalRequestBox
+import com.horizen.utils.{BlockFeeInfo, WithdrawalEpochInfo}
+
+trait StateReader extends scorex.core.transaction.state.StateReader {
+
+  def withdrawalRequests(withdrawalEpoch: Int): Seq[WithdrawalRequestBox]
+  def certificate(referencedWithdrawalEpoch: Int): Option[WithdrawalEpochCertificate]
+  def certificateTopQuality(referencedWithdrawalEpoch: Int): Long
+  def getWithdrawalEpochInfo: WithdrawalEpochInfo
+
+  def hasCeased: Boolean
+
+  // todo: consensus related part
+
+  // todo: fee payments related part
+  def getBlockFeePayments(withdrawalEpochNumber: Int): Seq[BlockFeeInfo]
+}
---
+++ sdk/src/main/scala/com/horizen/state/StateView.scala
@@ -0,0 +1,28 @@
+package com.horizen.state
+
+import com.horizen.block.{MainchainBlockReferenceData, WithdrawalEpochCertificate}
+import com.horizen.box.{ForgerBox, WithdrawalRequestBox}
+import com.horizen.transaction.Transaction
+import com.horizen.utils.{BlockFeeInfo, WithdrawalEpochInfo}
+import scorex.core.VersionTag
+
+import scala.util.Try
+
+trait StateView[TX <: Transaction, SV <: StateView[TX, SV]] extends StateReader {
+  view: SV =>
+
+  def applyMainchainBlockReferenceData(refData: MainchainBlockReferenceData): Try[SV]
+  def applyTransaction(tx: TX): Try[SV]
+
+  def addCertificate(cert: WithdrawalEpochCertificate): Try[SV]
+  def addWithdrawalRequest(wrb: WithdrawalRequestBox): Try[SV]
+  def delegateStake(fb: ForgerBox): Try[SV] // todo
+  def spendStake(fb: ForgerBox): Try[SV] // todo
+  def addFeeInfo(info: BlockFeeInfo): Try[SV]
+  def updateWithdrawalEpochInfo(withdrawalEpochInfo: WithdrawalEpochInfo): Try[SV]
+  def setCeased(): Try[SV]
+
+  def savepoint(): Unit
+  def rollbackToSavepoint(): Try[SV]
+  def commit(version: VersionTag): Try[Unit] // todo
+}
