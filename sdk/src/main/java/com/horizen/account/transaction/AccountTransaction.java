8885899ef53ffbff6e6d8014a50a0c799ed8a9e4
akrckn <alexander@rocknitive.com>
2022-05-20 10:05:13 +0200

    reformat

--- sdk/src/main/java/com/horizen/account/transaction/AccountTransaction.java
+++ sdk/src/main/java/com/horizen/account/transaction/AccountTransaction.java
@@ -9,12 +9,20 @@ import java.math.BigInteger;
 
 public abstract class AccountTransaction<P extends Proposition, PR extends Proof<P>> extends Transaction {
     public abstract void semanticValidity() throws TransactionSemanticValidityException;
+
     public abstract BigInteger getNonce();
+
     public abstract BigInteger getGasPrice();
+
     public abstract BigInteger getGasLimit();
+
     public abstract P getFrom();
+
     public abstract P getTo();
+
     public abstract BigInteger getValue();
+
     public abstract String getData();
+
     public abstract PR getSignature();
 }
