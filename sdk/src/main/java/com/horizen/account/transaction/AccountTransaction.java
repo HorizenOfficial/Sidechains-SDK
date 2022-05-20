7f59c28534a476bfd02df991048c79ce2c7411c7
akrckn <alexander@rocknitive.com>
2022-05-20 10:04:02 +0200

    add abstract account transaction

--- sdk/src/main/java/com/horizen/account/transaction/AccountTransaction.java
+++ sdk/src/main/java/com/horizen/account/transaction/AccountTransaction.java
@@ -5,6 +5,16 @@ import com.horizen.proposition.Proposition;
 import com.horizen.transaction.Transaction;
 import com.horizen.transaction.exception.TransactionSemanticValidityException;
 
+import java.math.BigInteger;
+
 public abstract class AccountTransaction<P extends Proposition, PR extends Proof<P>> extends Transaction {
     public abstract void semanticValidity() throws TransactionSemanticValidityException;
+    public abstract BigInteger getNonce();
+    public abstract BigInteger getGasPrice();
+    public abstract BigInteger getGasLimit();
+    public abstract P getFrom();
+    public abstract P getTo();
+    public abstract BigInteger getValue();
+    public abstract String getData();
+    public abstract PR getSignature();
 }
