0fdfd9e6ed690506936851ab8fd7d9cb7f1e4753
Jan Christoph Bernack <jc.bernack@gmail.com>
2022-05-25 01:53:06 +0200

    refactor check of InteropResult

--- evm/src/main/java/com/horizen/evm/StateDB.java
+++ evm/src/main/java/com/horizen/evm/StateDB.java
@@ -6,31 +6,27 @@ public class StateDB implements AutoCloseable {
     private final int handle;
 
     public StateDB(String stateRootHex) throws Exception {
-        var result = Evm.Instance.OpenState(stateRootHex);
-        if (result.code != 0) {
-            throw new Exception(result.message);
-        }
-        handle = result.handle;
+        handle = check(Evm.Instance.OpenState(stateRootHex)).handle;
     }
 
     public String GetIntermediateStateRoot() throws Exception {
-        var result = Evm.Instance.GetIntermediateStateRoot(handle);
-        if (result.code != 0) {
-            throw new Exception(result.message);
-        }
-        return result.stateRoot;
+        return check(Evm.Instance.GetIntermediateStateRoot(handle)).stateRoot;
     }
 
     @Override
     public void close() throws Exception {
-        var result = Evm.Instance.CloseState(handle);
-        if (result.code != 0) {
-            throw new Exception(result.message);
-        }
+        check(Evm.Instance.CloseState(handle));
     }
 
     @Override
     public String toString() {
         return String.format("StateDB{handle=%d}", handle);
+    }
+
+    private <T extends Evm.InteropResult> T check(T result) throws Exception {
+        if (result.code != 0) {
+            throw new Exception(result.message);
+        }
+        return result;
     }
 }
