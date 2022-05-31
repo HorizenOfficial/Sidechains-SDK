67b12f632a0c23f14f849ce2741517c2efb8ae29
Jan Christoph Bernack <jc.bernack@gmail.com>
2022-05-31 23:42:29 +0200

    update StateDBTest

--- evm/src/test/java/com/horizen/evm/StateDBTest.java
+++ evm/src/test/java/com/horizen/evm/StateDBTest.java
@@ -5,8 +5,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.*;
 
 public class StateDBTest {
     @Rule
@@ -15,45 +14,54 @@ public class StateDBTest {
     @Test
     public void TestStateDB() throws Exception {
         var databaseFolder = tempFolder.newFolder("evm-db");
-        System.out.println("temporary database folder: " + databaseFolder.getAbsolutePath());
 
         String hashNull = "0x0000000000000000000000000000000000000000000000000000000000000000";
         String hashEmpty = "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421";
         String origin = "0xbafe3b6f2a19658df3cb5efca158c93272ff5c0b";
 
-        System.out.println("Initialize");
         LibEvm.Initialize(databaseFolder.getAbsolutePath());
 
-        System.out.println("OpenState");
+        String rootWithBalance1234;
+        String rootWithBalance802;
         try (var statedb = new StateDB(hashNull)) {
-            System.out.println("OpenState result " + statedb);
-
-            System.out.println("GetIntermediateStateRoot");
             var intermediateRoot = statedb.GetIntermediateRoot();
-            System.out.println("GetIntermediateStateRoot result " + intermediateRoot);
             assertEquals(
                 "empty state should give the hash of an empty string as the root hash",
                 intermediateRoot,
                 hashEmpty
             );
 
-            System.out.println("Commit");
             var committedRoot = statedb.Commit();
-            System.out.println("Commit result " + committedRoot);
             assertEquals("committed root should equal intermediate root", committedRoot, intermediateRoot);
+            assertEquals(statedb.GetBalance(origin), "0");
 
-            System.out.println("StateGetAccountBalance");
-            var balance = statedb.GetAccountBalance(origin);
-            System.out.println("StateGetAccountBalance result " + balance);
+            statedb.AddBalance(origin, "1234");
+            assertEquals(statedb.GetBalance(origin), "1234");
+            assertNotEquals(
+                "intermediate root should not equal committed root anymore",
+                statedb.GetIntermediateRoot(),
+                committedRoot
+            );
+            rootWithBalance1234 = statedb.Commit();
 
-            System.out.println("StateGetAccount");
-            var account = statedb.GetStateAccount(origin);
-            System.out.println("StateGetAccount result " + account);
+            statedb.SubBalance(origin, "432");
+            assertEquals(statedb.GetBalance(origin), "802");
+            rootWithBalance802 = statedb.Commit();
 
-            System.out.println("CloseState");
+            assertEquals(statedb.GetNonce(origin), 0);
+            statedb.SetNonce(origin, 3);
+            assertEquals(statedb.GetNonce(origin), 3);
         }
         // verify that automatic resource management worked and StateDB.close() was called,
         // if it did the handle is invalid now and this should throw
         assertThrows(Exception.class, () -> LibEvm.StateIntermediateRoot(1));
+
+        try (var statedb = new StateDB(rootWithBalance1234)) {
+            assertEquals(statedb.GetBalance(origin), "1234");
+        }
+
+        try (var statedb = new StateDB(rootWithBalance802)) {
+            assertEquals(statedb.GetBalance(origin), "802");
+        }
     }
 }
