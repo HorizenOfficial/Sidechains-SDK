e5c544c423ba48943345d53853055587ae2b6d2b
saratnt <sara@horizenlabs.io>
2022-06-10 18:02:20 +0200

    Add check for valid zen amount

--- sdk/src/main/scala/com/horizen/account/api/http/ZenConverter.scala
+++
@@ -1,9 +0,0 @@
-package com.horizen.account.api.http
-
-object ZenConverter {
-  val ZENNY_TO_WEI_MULTIPLIER: java.math.BigInteger =  java.math.BigInteger.valueOf(10000000000L)
-
-  def convertZenniesToWei(valueInZennies: Long): java.math.BigInteger = {
-    ZENNY_TO_WEI_MULTIPLIER.multiply(java.math.BigInteger.valueOf(valueInZennies))
-  }
-}
