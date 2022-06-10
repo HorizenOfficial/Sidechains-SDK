9a70d68c05ae5d718e1b8bc035beb8410d583a93
Yuri Sibirski <31861444+ysibirski@users.noreply.github.com>
2022-06-09 21:16:16 -0400

    Publish snapshots and set_version script (#396)
    Co-authored-by: lander86 <40528366+lander86@users.noreply.github.com>

---
+++ ci/set_version.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+set -euo pipefail
+set +x
+
+# Get the directory of the currently executing script and its parent dir
+current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+parent_dir="${current_dir%/*}"
+
+# pom xml files
+pom_xml_location="${parent_dir}"
+sdk_xml_location="${parent_dir}/sdk"
+dbtool_xml_location="${parent_dir}/tools/dbtool"
+simpleapp_xml_location="${parent_dir}/examples/simpleapp"
+sctool_xml_location="${parent_dir}/tools/sctool"
+
+# .sh file(s)
+run_sc_file="${parent_dir}/ci/run_sc.sh"
+
+# python file(s)
+scutil_file="${parent_dir}/qa/SidechainTestFramework/scutil.py"
+sc_test_framework_file="${parent_dir}/qa/SidechainTestFramework/sc_test_framework.py"
+
+# .md files
+mc_sc_workflow_file="${parent_dir}/examples/simpleapp/mc_sc_workflow_example.md"
+simpleapp_readme_file="${parent_dir}/examples/simpleapp/README.md"
+main_readme_file="${parent_dir}/README.md"
+
+# Functions
+function fn_die() {
+  echo -e "${1}" >&2
+  exit "${2:-1}"
+}
+
+function usage() {
+  cat << BLOCK
+  Usage: Provide OLD and NEW versions as the 1st and 2nd arguments respectively.
+         It has to match the following format:
+         DIGIT.DIGIT.DIGIT or DIGIT.DIGIT.DIGIT-SNAPSHOT
+
+         For example:
+         ./set_version.sh 5.5.5 5.5.5-SNAPSHOT
+         ./set_version.sh 5.5.5-SNAPSHOT 5.5.5
+BLOCK
+  fn_die "Exiting ..."
+}
+
+# Checking for exact amount of arguments as the first step
+if [[ $# -eq 2 ]]; then
+    version_old="${1}"
+    version_new="${2}"
+else
+    usage
+fi
+
+# Checking the format of the versions
+if ! [[ "${version_old}" =~ ^[0-9]+\.[0-9]+\.[0-9]+(-SNAPSHOT)?$ ]]; then
+  usage
+fi
+
+if ! [[ "${version_new}" =~ ^[0-9]+\.[0-9]+\.[0-9]+(-SNAPSHOT)?$ ]]; then
+  usage
+fi
+
+# Changing version numbers under pom.xml files
+for dir in "${pom_xml_location}" "${sdk_xml_location}" "${dbtool_xml_location}" "${simpleapp_xml_location}" "${sctool_xml_location}"; do
+  # Checking if OLD version matches with the CURRENT version in pom file(s)
+  cd "${dir}"
+  current_pom_version="$(mvn help:evaluate -q -Dexpression=project.version -DforceStdout 2>/dev/null)"
+  if [ "${version_old}" != "${current_pom_version}" ]; then
+    fn_die "Fix it! The OLD version does not match with CURRENT version under ${dir}/pom.xml file\nCurrent version is: ${current_pom_version}"
+  fi
+
+  echo "" && echo "=== Modifying pom file under ${dir} location ===" && echo ""
+
+  cd "${dir}" && mvn versions:set -DnewVersion="${version_new}"
+
+  # shellcheck disable=SC1014
+  if grep -cq '<artifactId>sidechains-sdk</artifactIds>' "${dir}/pom.xml"; then
+    mvn versions:use-dep-version -Dincludes=io.horizen:sidechains-sdk -DdepVersion="${version_new}" -DforceVersion=true
+  fi
+done
+
+# Changing version under other file(s)
+# shellcheck disable=SC2001
+version_old_dot_escaped="$(sed -e 's/\./\\./g' <<< "${version_old}")"
+echo "dotted version = $version_old_dot_escaped"
+for file in "${scutil_file}" "${sc_test_framework_file}" "${mc_sc_workflow_file}" "${simpleapp_readme_file}" "${main_readme_file}" "${run_sc_file}"; do
+  # Checking if OLD version matches with the CURRENT version in pom file(s)
+  if [ "${file}" != "${run_sc_file}" ]; then
+    if ! grep -cq "sidechains.*${version_old}" "${file}"; then
+      current_jar_version="$(grep -Eoh 'sidechains-sdk.*[0-9]+\.[0-9]+\.[0-9]+(-SNAPSHOT)?' "${file}" | cut -d '-' -f4- | head -n1)"
+      fn_die "Fix it! The OLD version does not match with CURRENT version under ${file} file\nCurrent version is: ${current_jar_version}"
+    fi
+  elif [ "${file}" = "${run_sc_file}" ];then
+    if ! grep -cq "SIMPLE_APP_VERSION:-${version_old}" "${file}"; then
+      current_jar_version="$(grep -Eioh 'SIMPLE_APP_VERSION:-[0-9]+\.[0-9]+\.[0-9]+(-SNAPSHOT)?' "${file}" | cut -d '-' -f2-)"
+      fn_die "Fix it! The OLD version does not match with CURRENT version under ${file} file\nCurrent version is: ${current_jar_version}"
+    fi
+  fi
+
+  echo "" && echo "=== Modifying ${file} file ===" && echo ""
+  sed -i "s/${version_old_dot_escaped}/${version_new}/g" "${file}"
+done
+
+echo "" && echo "=== DONE ===" && echo ""
+echo -e "OLD version: ${version_old}\nNEW version: ${version_new}"
+
+exit 0
+
--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -14,31 +14,67 @@ echo "./sdk/pom.xml version:                $pom_version"
 echo "./tools/sctool/pom.xml version:       $pom_version"
 
 export CONTAINER_PUBLISH="false"
+
+# Functions
+function import_gpg_keys() {
+  # shellcheck disable=SC2145
+  printf "%s\n" "Tagged build, fetching keys:" "${@}" ""
+  # shellcheck disable=SC2207
+  declare -r my_arr=( $(echo "${@}" | tr " " "\n") )
+
+  for key in "${my_arr[@]}"; do
+    echo "Importing key: ${key}"
+    gpg -v --batch --keyserver hkps://keys.openpgp.org --recv-keys "${key}" ||
+    gpg -v --batch --keyserver hkp://keyserver.ubuntu.com --recv-keys "${key}" ||
+    gpg -v --batch --keyserver hkp://pgp.mit.edu:80 --recv-keys "${key}" ||
+    gpg -v --batch --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys "${key}"
+  done
+}
+
+function check_signed_tag() {
+  # Checking if git tag signed by the maintainers
+  if git verify-tag -v "${1}"; then
+    echo "${1} is a valid signed tag"
+  else
+    echo "Git tag's = ${1} gpg signature is NOT valid. The build is not going to be released..."
+    return 99
+  fi
+}
+
 # empty key.asc file in case we're not signing
 touch "${HOME}/key.asc"
 
-if [ ! -z "${TRAVIS_TAG}" ]; then
+if [ -n "${TRAVIS_TAG}" ]; then
+  # checking if MAINTAINER_KEYS is set
+  if [ -z "${MAINTAINER_KEYS}" ]; then
+    echo "MAINTAINER_KEYS variable is not set. Make sure to set it up for release build!!!"
+    exit 1
+  fi
+
   export GNUPGHOME="$(mktemp -d 2>/dev/null || mktemp -d -t 'GNUPGHOME')"
-  echo "Tagged build, fetching maintainer keys."
-    gpg -v --batch --keyserver hkps://keys.openpgp.org --recv-keys $MAINTAINER_KEYS ||
-    gpg -v --batch --keyserver hkp://keyserver.ubuntu.com --recv-keys $MAINTAINER_KEYS ||
-    gpg -v --batch --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys $MAINTAINER_KEYS ||
-    gpg -v --batch --keyserver hkp://ipv4.pool.sks-keyservers.net --recv-keys $MAINTAINER_KEYS ||
-    gpg -v --batch --keyserver hkp://pgp.mit.edu:80 --recv-keys $MAINTAINER_KEYS
-  if git verify-tag -v "${TRAVIS_TAG}"; then
-    echo "Valid signed tag"
+  # shellcheck disable=SC2086
+  import_gpg_keys ${MAINTAINER_KEYS}
+
+  # Checking git tag gpg signature requirement
+  if (check_signed_tag "${TRAVIS_TAG}"); then
+    echo "" && echo "=== Release build ===" && echo ""
+
     if [ "${pom_version}" != "$simpleapp_version" ] || [ "${pom_version}" != "$sdk_version" ] || [ "${pom_version}" != "$sctool_version" ]; then
-      echo "Aborting, mistmatch in at least one pom.xml version number."
+      echo "Aborting, mismatch in at least one of the pom.xml version number."
+      exit 1
+    elif ! [[ "${pom_version}" =~ ^[0-9]+\.[0-9]+\.[0-9]+(-SNAPSHOT)?$ ]]; then
+      echo "Aborting, pom version is in the wrong format."
       exit 1
     fi
-    if [ "${TRAVIS_TAG}" != "${pom_version}" ]; then
-      echo "Aborting, tag differs from the pom file."
+
+    if ! [[ "${TRAVIS_TAG}" =~ "${pom_version}"[0-9]*$ ]]; then
+      echo "Aborting, tag format differs from the pom file."
       exit 1
     else
       export CONTAINER_PUBLISH="true"
-      echo "Fetching gpg signing keys."
-      curl -sLH "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3.raw" "$MAVEN_KEY_ARCHIVE_URL" |
-        openssl enc -d -aes-256-cbc -md sha256 -pass pass:$MAVEN_KEY_ARCHIVE_PASSWORD |
+      echo "Fetching maven gpg signing keys."
+      curl -sLH "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3.raw" "${MAVEN_KEY_ARCHIVE_URL}" |
+        openssl enc -d -aes-256-cbc -md sha256 -pass pass:"${MAVEN_KEY_ARCHIVE_PASSWORD}" |
         tar -xzf- -C "${HOME}"
     fi
   fi
@@ -54,6 +90,7 @@ if [ "${CONTAINER_PUBLISH}" = "false" ]; then
   unset CONTAINER_OSSRH_JIRA_PASSWORD
   unset CONTAINER_GPG_KEY_NAME
   unset CONTAINER_GPG_PASSPHRASE
+  echo "" && echo "=== NOT a release build ===" && echo ""
 fi
 
 # unset credentials after use
