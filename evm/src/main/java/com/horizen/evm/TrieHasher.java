abbbbcc8f6be42ff848556833ea2beffaf379406
Jan Christoph Bernack <jc.bernack@gmail.com>
2022-06-29 16:15:43 +0200

    add TrieHasher interop class

---
+++ evm/src/main/java/com/horizen/evm/TrieHasher.java
@@ -0,0 +1,10 @@
+package com.horizen.evm;
+
+public final class TrieHasher {
+    private TrieHasher() {
+    }
+
+    public static byte[] Root(byte[][] values) {
+        return LibEvm.hashRoot(values);
+    }
+}
