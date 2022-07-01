e14191db640ed99014e0ff8fce9aa3461eda2813
cronicc <cronic@zensystem.io>
2022-07-01 11:36:13 +0200

    Update ci/devtools/lint_pom.xml.sh

--- ci/devtools/lint_pom.xml.sh
+++ ci/devtools/lint_pom.xml.sh
@@ -2,7 +2,7 @@
 
 base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../..";
 
-for location in "${base_dir}" "${base_dir}/skd" "${base_dir}/tools/dbtool" "${base_dir}/tools/sctool" "${base_dir}/examples/simpleapp"; do
+for location in "${base_dir}" "${base_dir}/sdk" "${base_dir}/tools/dbtool" "${base_dir}/tools/sctool" "${base_dir}/examples/simpleapp"; do
   CONTENT="$(xmllint --format --encode UTF-8 "${location}"/pom.xml)"
   echo "${CONTENT}" > "${location}/pom.xml"
 done
