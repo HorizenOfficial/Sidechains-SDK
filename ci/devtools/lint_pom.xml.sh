06a74866dac84a46942fb993e7d74b52c8da46a8
Yuri Sibirski <yuri@horizenlabs.io>
2022-07-06 11:38:52 -0400

    Ported changes from dev_evm branch

--- ci/devtools/lint_pom.xml.sh
+++ ci/devtools/lint_pom.xml.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../..";
+pom_xml_locations=("${base_dir}" "${base_dir}/sdk" "${base_dir}/examples/simpleapp" "${base_dir}/tools/sctool")
 
-for location in "${base_dir}" "${base_dir}/sdk" "${base_dir}/tools/dbtool" "${base_dir}/tools/sctool" "${base_dir}/examples/simpleapp"; do
+# Conditional for evm branch
+if [ -d "${base_dir}/libevm" ]; then
+  pom_xml_locations+=("${base_dir}/evm" "${base_dir}/libevm" "${base_dir}/examples/evmapp")
+else
+  pom_xml_locations+=("${base_dir}/tools/dbtool")
+fi
+
+for location in "${pom_xml_locations[@]}"; do
   CONTENT="$(xmllint --format --encode UTF-8 "${location}"/pom.xml)"
   echo "${CONTENT}" > "${location}/pom.xml"
 done
