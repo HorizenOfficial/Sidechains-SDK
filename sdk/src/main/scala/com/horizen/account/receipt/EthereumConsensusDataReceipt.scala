6a3fbb95f5b73324b4d59236fe72b9a44ec34cbc
GitMark0 <marko.ivankovic650@gmail.com>
2022-08-22 11:44:44 +0200

    add functions for bloom filter manipulation

--- sdk/src/main/scala/com/horizen/account/receipt/EthereumConsensusDataReceipt.scala
+++ sdk/src/main/scala/com/horizen/account/receipt/EthereumConsensusDataReceipt.scala
@@ -6,9 +6,10 @@ import com.horizen.account.receipt.ReceiptTxType.ReceiptTxType
 import com.horizen.evm.interop.EvmLog
 import com.horizen.utils.BytesUtils
 import org.web3j.rlp._
+import scorex.crypto.hash.Keccak256
 
 import java.math.BigInteger
-import java.nio.ByteBuffer
+import java.nio.{ByteBuffer, ByteOrder}
 import java.util
 import scala.collection.mutable.ListBuffer
 
@@ -36,7 +37,7 @@ case class EthereumConsensusDataReceipt(
   }
 
   require(transactionType >= 0 && transactionType <= ReceiptTxType.DynamicFeeTxType.id)
-  require(status == ReceiptStatus.SUCCESSFUL.id || status == ReceiptStatus.FAILED.id )
+  require(status == ReceiptStatus.SUCCESSFUL.id || status == ReceiptStatus.FAILED.id)
 
   def getTxType: ReceiptTxType = {
     transactionType match {
@@ -63,14 +64,14 @@ case class EthereumConsensusDataReceipt(
       case other: EthereumConsensusDataReceipt =>
         transactionType == other.transactionType &&
           status == other.status &&
-        cumulativeGasUsed.equals(other.cumulativeGasUsed) &&
+          cumulativeGasUsed.equals(other.cumulativeGasUsed) &&
           logs.toSet == other.logs.toSet
 
       case _ => false
     }
   }
 
-  override def hashCode: Int =  {
+  override def hashCode: Int = {
     var result = Integer.hashCode(transactionType)
     result = 31 * result + Integer.hashCode(status)
     result = 31 * result + cumulativeGasUsed.hashCode()
@@ -103,13 +104,12 @@ object ReceiptTxType extends Enumeration {
   val LegacyTxType, AccessListTxType, DynamicFeeTxType = Value
 }
 
-object EthereumConsensusDataReceipt{
+object EthereumConsensusDataReceipt {
 
   object ReceiptStatus extends Enumeration {
     type ReceiptStatus = Value
     val FAILED, SUCCESSFUL = Value
   }
-
 
 
   def decodeLegacy(rlpData: Array[Byte]): EthereumConsensusDataReceipt = {
@@ -188,54 +188,41 @@ object EthereumConsensusDataReceipt{
     result
   }
 
-  def createLogsBloom(receipt : Seq[EvmLog]): Array[Byte] = {
-    // we can create bloom filter out of a log or out of a receipt log list 
-    /* see: https://github.com/ethereum/go-ethereum/blob/55f914a1d764dac4bd37a48173092b1f5c3b186d/core/types/bloom9.go
-
-                // CreateBloom creates a bloom filter out of the give Receipts (+Logs)
-                func CreateBloom(receipts Receipts) Bloom {
-                    buf := make([]byte, 6)
-                    var bin Bloom
-                    for _, receipt := range receipts {
-                        for _, log := range receipt.Logs {
-                            bin.add(log.Address.Bytes(), buf)
-                            for _, b := range log.Topics {
-                                bin.add(b[:], buf)
-                            }
-                        }
-                    }
-                    return bin
-                }
-
-                // add is internal version of Add, which takes a scratch buffer for reuse (needs to be at least 6 bytes)
-                func (b *Bloom) add(d []byte, buf []byte) {
-                    i1, v1, i2, v2, i3, v3 := bloomValues(d, buf)
-                    b[i1] |= v1
-                    b[i2] |= v2
-                    b[i3] |= v3
-                }
-
-                // bloomValues returns the bytes (index-value pairs) to set for the given data
-                func bloomValues(data []byte, hashbuf []byte) (uint, byte, uint, byte, uint, byte) {
-                    sha := hasherPool.Get().(crypto.KeccakState)
-                    sha.Reset()
-                    sha.Write(data)
-                    sha.Read(hashbuf)
-                    hasherPool.Put(sha)
-                    // The actual bits to flip
-                    v1 := byte(1 << (hashbuf[1] & 0x7))
-                    v2 := byte(1 << (hashbuf[3] & 0x7))
-                    v3 := byte(1 << (hashbuf[5] & 0x7))
-                    // The indices for the bytes to OR in
-                    i1 := BloomByteLength - uint((binary.BigEndian.Uint16(hashbuf)&0x7ff)>>3) - 1
-                    i2 := BloomByteLength - uint((binary.BigEndian.Uint16(hashbuf[2:])&0x7ff)>>3) - 1
-                    i3 := BloomByteLength - uint((binary.BigEndian.Uint16(hashbuf[4:])&0x7ff)>>3) - 1
-
-                    return i1, v1, i2, v2, i3, v3
-                }
-
-             */
-    // TODO shall we use libevm implementation?
-    new Array[Byte](256)
+  def bytesToShort(byte1: Byte, byte2: Byte): Short = {
+    ByteBuffer
+      .allocate(2)
+      .order(ByteOrder.BIG_ENDIAN)
+      .put(byte1)
+      .put(byte2)
+      .getShort(0)
   }
+
+  def getBloomFilterValues(data: Array[Byte]) = {
+    // Copy first 3 pairs of bytes to hashBuffer
+    val hashBuffer = Keccak256.hash(data)
+    println(String.format("%02x", Byte.box(hashBuffer(1))))
+
+    // Calculate byte values
+    val v1 = 1 << (hashBuffer(1) & 0x7)
+    val v2 = 1 << (hashBuffer(3) & 0x7)
+    val v3 = 1 << (hashBuffer(5) & 0x7)
+
+    // Calculate byte indexes
+    val i1 = 256 - ((bytesToShort(hashBuffer(0), hashBuffer(1)) & 0x7ff) >> 3) - 1
+    val i2 = 256 - ((bytesToShort(hashBuffer(2), hashBuffer(3)) & 0x7ff) >> 3) - 1
+    val i3 = 256 - ((bytesToShort(hashBuffer(4), hashBuffer(5)) & 0x7ff) >> 3) - 1
+
+    (i1, v1, i2, v2, i3, v3)
+  }
+
+  def addDataToBloomFilter(bloomFilter: Array[Byte], data: Array[Byte]): Array[Byte] = {
+    val (i1, v1, i2, v2, i3, v3) = getBloomFilterValues(data)
+
+    bloomFilter(i1) = (bloomFilter(i1) | v1).toByte
+    bloomFilter(i2) = (bloomFilter(i2) | v2).toByte
+    bloomFilter(i3) = (bloomFilter(i3) | v3).toByte
+
+    bloomFilter
+  }
+
 }
