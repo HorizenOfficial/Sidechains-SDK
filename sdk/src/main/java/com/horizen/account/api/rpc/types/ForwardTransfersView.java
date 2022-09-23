b403c7d699991da54a52bf58758702155259a18f
akrckn <alexander@rocknitive.com>
2022-09-23 12:13:57 +0200

    simplify forward transfers view, add tests for forward transfer api methods

--- sdk/src/main/java/com/horizen/account/api/rpc/types/ForwardTransfersView.java
+++ sdk/src/main/java/com/horizen/account/api/rpc/types/ForwardTransfersView.java
@@ -1,13 +1,15 @@
 package com.horizen.account.api.rpc.types;
 
 import com.fasterxml.jackson.annotation.JsonView;
-import com.horizen.evm.utils.Address;
+import com.horizen.account.proposition.AddressProposition;
+import com.horizen.account.utils.MainchainTxCrosschainOutputAddressUtil;
+import com.horizen.account.utils.ZenWeiConverter;
+import com.horizen.block.MainchainTxForwardTransferCrosschainOutput;
 import com.horizen.serialization.Views;
 import com.horizen.transaction.mainchain.ForwardTransfer;
 import org.web3j.utils.Numeric;
 
 import java.math.BigInteger;
-import java.util.Arrays;
 import java.util.List;
 import java.util.TreeMap;
 
@@ -16,15 +18,21 @@ public class ForwardTransfersView {
     private final TreeMap<Integer, ForwardTransferData> forwardTransfers = new TreeMap();
 
     public ForwardTransfersView(List<ForwardTransfer> transactions, boolean noPrefix) {
-        if (transactions.size() > 0) {
-            if (noPrefix)
-                transactions.forEach(txOutput -> forwardTransfers.put(forwardTransfers.size(),
-                        new ForwardTransferData(Numeric.toHexStringNoPrefix(Arrays.copyOf(txOutput.getFtOutput().propositionBytes(), Address.LENGTH)),
-                                String.valueOf(txOutput.getFtOutput().amount()))));
-            else
-                transactions.forEach(txOutput -> forwardTransfers.put(forwardTransfers.size(),
-                        new ForwardTransferData(Numeric.toHexString(Arrays.copyOf(txOutput.getFtOutput().propositionBytes(), Address.LENGTH)),
-                                Numeric.toHexStringWithPrefix(BigInteger.valueOf(txOutput.getFtOutput().amount())))));
+        for (int i = 0; i < transactions.size(); i++) {
+            MainchainTxForwardTransferCrosschainOutput ftOutput = transactions.get(i).getFtOutput();
+            var to = "";
+            var value = "";
+            AddressProposition address = new AddressProposition(
+                    MainchainTxCrosschainOutputAddressUtil.getAccountAddress(ftOutput.propositionBytes()));
+            BigInteger weiValue = ZenWeiConverter.convertZenniesToWei(ftOutput.amount());
+            if (noPrefix) {
+                to = Numeric.toHexStringNoPrefix(address.address());
+                value = String.valueOf(weiValue);
+            } else {
+                to = Numeric.toHexString(address.address());
+                value = Numeric.toHexStringWithPrefix(weiValue);
+            }
+            forwardTransfers.put(i, new ForwardTransferData(to, value));
         }
     }
 
