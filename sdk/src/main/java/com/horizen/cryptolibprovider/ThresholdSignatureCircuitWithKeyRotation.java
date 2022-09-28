3f9691299cd665a4944e42b780c89a19a626fef0
sgonta <sgonta@horizenlabs.io>
2022-09-28 11:00:22 +0300

    ThresholdSignatureCircuitWithKeyRotation
    Signed-off-by: sgonta <sgonta@horizenlabs.io>

--- sdk/src/main/java/com/horizen/cryptolibprovider/ThresholdSignatureCircuitWithKeyRotation.java
+++ sdk/src/main/java/com/horizen/cryptolibprovider/ThresholdSignatureCircuitWithKeyRotation.java
@@ -1,11 +1,11 @@
 package com.horizen.cryptolibprovider;
 
+import com.horizen.block.WithdrawalEpochCertificate;
 import com.horizen.box.WithdrawalRequestBox;
 import com.horizen.utils.Pair;
 import scala.collection.Seq;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ThresholdSignatureCircuitWithKeyRotation {
     Pair<byte[], Long> createProof(List<WithdrawalRequestBox> bt,
@@ -15,7 +15,12 @@ public interface ThresholdSignatureCircuitWithKeyRotation {
                                    long btrFee,
                                    long ftMinAmount,
                                    Seq<byte[]> customParameters,
-                                   signatures1 : seq,
-                                   signatures2 : seq,
-                                   signatures3 : seq);
+                                   Seq<byte[]> signingKeySignatures,
+                                   Seq<byte[]> masterKeySignatures,
+                                   Seq<byte[]> newKeySignatures,
+                                   WithdrawalEpochCertificate previousCertificate,
+                                   long threshold,
+                                   String provingKeyPath,
+                                   boolean checkProvingKey,
+                                   boolean zk); // map fields to createProof of Cryptolib
 }
