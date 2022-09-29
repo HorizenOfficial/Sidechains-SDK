33be0fb37a77a9be6285a683b0f49ff5a39ac53b
sgonta <sgonta@horizenlabs.io>
2022-09-29 23:05:57 +0300

    Key rotation circuit
    Signed-off-by: sgonta <sgonta@horizenlabs.io>

---
+++ sdk/src/main/java/com/horizen/cryptolibprovider/KeyRotationCircuit.java
@@ -0,0 +1,55 @@
+package com.horizen.cryptolibprovider;
+
+import com.horizen.box.WithdrawalRequestBox;
+import com.horizen.utils.Pair;
+import scala.collection.Seq;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface KeyRotationCircuit {
+    byte[] generateMessageToBeSigned(List<WithdrawalRequestBox> bt,
+                                     byte[] sidechainId,
+                                     int epochNumber,
+                                     byte[] endCumulativeScTxCommTreeRoot,
+                                     long btrFee,
+                                     long ftMinAmount,
+                                     Seq<byte[]> customParameters);
+
+    //None elements for schnorrSignatureBytesList if no secret key available, thus schnorrSignatureBytesList.size() == schnorrPublicKeysBytesList.size()
+    //threshold is the same as in generateSysDataConstant
+    Pair<byte[], Long> createProof(List<WithdrawalRequestBox> bt,
+                                   byte[] sidechainId,
+                                   int epochNumber,
+                                   byte[] endCumulativeScTxCommTreeRoot,
+                                   long btrFee,
+                                   long ftMinAmount,
+                                   Seq<byte[]> customParameters,
+                                   List<Optional<byte[]>> schnorrSignatureBytesList,
+                                   List<byte[]> schnorrPublicKeysBytesList,
+                                   long threshold,
+                                   String provingKeyPath,
+                                   boolean checkProvingKey,
+                                   boolean zk); // todo check name
+
+    Boolean verifyProof(List<WithdrawalRequestBox> bt,
+                        byte[] sidechainId,
+                        int epochNumber,
+                        byte[] endCumulativeScTxCommTreeRoot,
+                        long btrFee,
+                        long ftMinAmount,
+                        Seq<byte[]> customFields,
+                        byte[] constant,
+                        long quality, byte[] proof,
+                        boolean checkProof,
+                        String verificationKeyPath,
+                        boolean checkVerificationKey);
+
+    byte[] generateSysDataConstant(List<byte[]> publicKeysList, long threshold);
+
+    boolean generateCoboundaryMarlinSnarkKeys(long maxPks, String provingKeyPath, String verificationKeyPath, int customFieldsNum);
+
+    List<byte[]> getCertificateCustomFields(Seq<byte[]> customFields);
+
+    byte[] reconstructUtxoMerkleTreeRoot(byte[] fe1Bytes, byte[] fe2Bytes);
+}
