28f45ba74b83d9c5542d42c7c292a17e5ee73f90
sgonta <sgonta@horizenlabs.io>
2022-10-06 16:31:51 +0300

    Master key signatures

--- sdk/src/main/scala/com/horizen/certificatesubmitter/dataproof/DataForProofGenerationWithKeyRotation.scala
+++ sdk/src/main/scala/com/horizen/certificatesubmitter/dataproof/DataForProofGenerationWithKeyRotation.scala
@@ -19,7 +19,11 @@ case class DataForProofGenerationWithKeyRotation(override val referencedEpochNum
                                                  schnorrMastersPublicKeysBytesList: Seq[Array[Byte]],
                                                  newSchnorrSignersPublicKeysBytesList: Seq[Array[Byte]],
                                                  newSchnorrMastersPublicKeysBytesList: Seq[Array[Byte]],
-                                                 val previousCertificateOption: Option[WithdrawalEpochCertificate])
+                                                 updatedSigningKeysSkSignatures: Seq[Array[Byte]],
+                                                 updatedSigningKeysMkSignatures: Seq[Array[Byte]],
+                                                 updatedMasterKeysSkSignatures: Seq[Array[Byte]],
+                                                 updatedMasterKeysMkSignatures: Seq[Array[Byte]],
+                                                 previousCertificateOption: Option[WithdrawalEpochCertificate])
   extends DataForProofGeneration(referencedEpochNumber, sidechainId, withdrawalRequests, endEpochCumCommTreeHash, btrFee, ftMinAmount, customFields, schnorrKeyPairs) {
   override def toString: String = {
     "DataForProofGeneration(" +
@@ -35,6 +39,10 @@ case class DataForProofGenerationWithKeyRotation(override val referencedEpochNum
       s"masters public keys = ${schnorrMastersPublicKeysBytesList.map(BytesUtils.toHexString)}), " +
       s"new signers public keys = ${newSchnorrSignersPublicKeysBytesList.map(BytesUtils.toHexString)}), " +
       s"new masters public keys = ${newSchnorrMastersPublicKeysBytesList.map(BytesUtils.toHexString)}), " +
+      s"updated signers keys signing key signatures = ${updatedSigningKeysSkSignatures.map(BytesUtils.toHexString)}), " +
+      s"updated signers keys master key signatures = ${updatedSigningKeysMkSignatures.map(BytesUtils.toHexString)}), " +
+      s"updated master keys signing key signatures = ${updatedMasterKeysSkSignatures.map(BytesUtils.toHexString)}), " +
+      s"updated master keys master key signatures = ${updatedMasterKeysMkSignatures.map(BytesUtils.toHexString)}), " +
       s"previous certificate = ${previousCertificateOption.getOrElse("").toString})"
   }
 }
