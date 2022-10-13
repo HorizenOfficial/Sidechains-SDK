6941f8c0e8cb0179659c0d254da3d0f6cdb71c1e
sgonta <sgonta@horizenlabs.io>
2022-10-13 10:14:06 +0300

    Proof creation refactoring

--- sdk/src/main/scala/com/horizen/certificatesubmitter/keys/SchnorrKeysSignaturesListBytes.scala
+++ sdk/src/main/scala/com/horizen/certificatesubmitter/keys/SchnorrKeysSignaturesListBytes.scala
@@ -1,5 +1,9 @@
 package com.horizen.certificatesubmitter.keys
 
+import com.horizen.schnorrnative.{SchnorrKeysSignaturesList, SchnorrPublicKey, SchnorrSignature}
+
+import java.util
+
 case class SchnorrKeysSignaturesListBytes(
                                       schnorrSignersPublicKeysBytesList: Seq[Array[Byte]],
                                       schnorrMastersPublicKeysBytesList: Seq[Array[Byte]],
@@ -10,3 +14,24 @@ case class SchnorrKeysSignaturesListBytes(
                                       updatedMasterKeysSkSignatures: Seq[Array[Byte]],
                                       updatedMasterKeysMkSignatures: Seq[Array[Byte]],
                                     )
+
+object SchnorrKeysSignaturesListBytes{
+  def getSchnorrKeysSignaturesList(schnorrKeysSignaturesListBytes: SchnorrKeysSignaturesListBytes): SchnorrKeysSignaturesList = {
+    new SchnorrKeysSignaturesList(
+      byteArrayToKeysList(schnorrKeysSignaturesListBytes.schnorrSignersPublicKeysBytesList),
+      byteArrayToKeysList(schnorrKeysSignaturesListBytes.schnorrMastersPublicKeysBytesList),
+      byteArrayToKeysList(schnorrKeysSignaturesListBytes.newSchnorrSignersPublicKeysBytesList),
+      byteArrayToKeysList(schnorrKeysSignaturesListBytes.newSchnorrMastersPublicKeysBytesList),
+      byteArrayToSignaturesList(schnorrKeysSignaturesListBytes.updatedSigningKeysSkSignatures),
+      byteArrayToSignaturesList(schnorrKeysSignaturesListBytes.updatedSigningKeysMkSignatures),
+      byteArrayToSignaturesList(schnorrKeysSignaturesListBytes.updatedMasterKeysSkSignatures),
+      byteArrayToSignaturesList(schnorrKeysSignaturesListBytes.updatedMasterKeysMkSignatures)
+    )
+  }
+
+  private def byteArrayToKeysList(schnorrPublicKeysBytesList: Seq[Array[Byte]]): util.List[SchnorrPublicKey] =
+    scala.collection.JavaConverters.seqAsJavaList(schnorrPublicKeysBytesList.map(SchnorrPublicKey.deserialize))
+
+  private def byteArrayToSignaturesList(schnorrSignaturesBytesList: Seq[Array[Byte]]): util.List[SchnorrSignature] =
+    scala.collection.JavaConverters.seqAsJavaList(schnorrSignaturesBytesList.map(SchnorrSignature.deserialize))
+}
