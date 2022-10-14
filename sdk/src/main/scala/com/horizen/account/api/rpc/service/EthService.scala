82beb831396a37cbd0ab2093263c2766b2509ec2
Jan Christoph Bernack <jc.bernack@gmail.com>
2022-10-14 12:55:37 +0200

    cleanup

--- sdk/src/main/scala/com/horizen/account/api/rpc/service/EthService.scala
+++ sdk/src/main/scala/com/horizen/account/api/rpc/service/EthService.scala
@@ -278,7 +278,7 @@ class EthService(
       // Execute the binary search and hone in on an executable gas limit
       // We need to do a search because the gas required during execution is not necessarily equal to the consumed
       // gas after the execution. See https://github.com/ethereum/go-ethereum/commit/682875adff760a29a2bb0024190883e4b4dd5d72
-      val requiredGasLimit = binarySearch(lowBound, highBound)(x => check(x)._1)
+      val requiredGasLimit = binarySearch(lowBound, highBound)(check(_)._1)
       // Reject the transaction as invalid if it still fails at the highest allowance
       if (requiredGasLimit == highBound) {
         val (success, reverted) = check(highBound)
