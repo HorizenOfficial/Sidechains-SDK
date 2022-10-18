5baf5f7489b05dedbf46c895d67bd23380692b37
sgonta <sgonta@horizenlabs.io>
2022-10-18 18:52:52 +0300

    Transaction parameters

--- sdk/src/main/java/com/horizen/transaction/KeyRotationTransaction.java
+++ sdk/src/main/java/com/horizen/transaction/KeyRotationTransaction.java
@@ -5,14 +5,18 @@ import com.horizen.box.BoxUnlocker;
 import com.horizen.box.ZenBox;
 import com.horizen.box.data.ZenBoxData;
 import com.horizen.certificatesubmitter.keys.KeyRotationProof;
+import com.horizen.certificatesubmitter.keys.KeyRotationProofType;
 import com.horizen.proof.Proof;
+import com.horizen.proof.SchnorrProof;
 import com.horizen.proof.Signature25519;
 import com.horizen.proposition.PublicKey25519Proposition;
+import com.horizen.proposition.SchnorrProposition;
 import com.horizen.secret.PrivateKey25519;
 import com.horizen.secret.Secret;
 import com.horizen.transaction.exception.TransactionSemanticValidityException;
 import com.horizen.utils.Pair;
 import scala.Array;
+import scala.Enumeration;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -24,8 +28,7 @@ import static com.horizen.transaction.CoreTransactionsIdsEnum.OpenStakeTransacti
 /*
  * KeyRotationTransaction is used for key rotation of signer or master key of certificate submitter
  */
-public class KeyRotationTransaction extends SidechainNoncedTransaction<PublicKey25519Proposition, ZenBox, ZenBoxData>
-{
+public class KeyRotationTransaction extends SidechainNoncedTransaction<PublicKey25519Proposition, ZenBox, ZenBoxData> {
     public final static byte KEY_ROTATION_TRANSACTION_VERSION = 1;
 
     final byte[] inputId;
@@ -56,7 +59,7 @@ public class KeyRotationTransaction extends SidechainNoncedTransaction<PublicKey
 
     @Override
     public synchronized List<BoxUnlocker<PublicKey25519Proposition>> unlockers() {
-        if(unlockers == null) {
+        if (unlockers == null) {
             unlockers = new ArrayList<>();
             BoxUnlocker<PublicKey25519Proposition> unlocker = new BoxUnlocker() {
                 @Override
@@ -76,7 +79,7 @@ public class KeyRotationTransaction extends SidechainNoncedTransaction<PublicKey
     }
 
     @Override
-    protected List<ZenBoxData> getOutputData(){
+    protected List<ZenBoxData> getOutputData() {
         ArrayList<ZenBoxData> output = new ArrayList();
         outputData.ifPresent(output::add);
         return output;
@@ -123,7 +126,9 @@ public class KeyRotationTransaction extends SidechainNoncedTransaction<PublicKey
     }
 
     @Override
-    public Boolean isCustom() { return false; }
+    public Boolean isCustom() {
+        return false;
+    }
 
     @Override
     public byte[] customFieldsData() {
@@ -135,9 +140,13 @@ public class KeyRotationTransaction extends SidechainNoncedTransaction<PublicKey
         return Ints.toByteArray(this.keyRotationProof.index());
     }
 
-    public int getForgerIndex() { return this.keyRotationProof.index(); }
+    public int getForgerIndex() {
+        return this.keyRotationProof.index();
+    }
 
-    public byte[] getInputId() { return this.inputId; }
+    public byte[] getInputId() {
+        return this.inputId;
+    }
 
     public Optional<ZenBoxData> getOutputBox() {
         return this.outputData;
@@ -149,16 +158,22 @@ public class KeyRotationTransaction extends SidechainNoncedTransaction<PublicKey
 
 
     public static KeyRotationTransaction create(Pair<ZenBox, PrivateKey25519> from,
-                                              PublicKey25519Proposition changeAddress,
-                                              int forgerIndex,
-                                              long fee) throws TransactionSemanticValidityException {
-        if(from == null)
+                                                PublicKey25519Proposition changeAddress,
+                                                int forgerIndex,
+                                                long fee,
+                                                int keyTypeEnumerationNumber,
+                                                int indexOfKey,
+                                                SchnorrProposition newValueOfKey,
+                                                SchnorrProof signingKeySignature,
+                                                SchnorrProof masterKeySignature
+    ) throws TransactionSemanticValidityException {
+        if (from == null)
             throw new IllegalArgumentException("Parameters can't be null.");
-        if(from.getKey().value() < fee )
+        if (from.getKey().value() < fee)
             throw new IllegalArgumentException("Fee can't be greater than the input!");
 
         Optional<ZenBoxData> output = Optional.empty();
-        if(from.getKey().value() > fee) {
+        if (from.getKey().value() > fee) {
             output = Optional.of(new ZenBoxData(changeAddress, from.getKey().value() - fee));
         }
         OpenStakeTransaction unsignedTransaction = new OpenStakeTransaction(from.getKey().id(), output, null, forgerIndex, fee, KEY_ROTATION_TRANSACTION_VERSION);
@@ -166,8 +181,8 @@ public class KeyRotationTransaction extends SidechainNoncedTransaction<PublicKey
         byte[] messageToSign = unsignedTransaction.messageToSign();
         Secret secret = from.getValue();
 
-        KeyRotationProof keyRotationProof = new KeyRotationProof(null, 0, null, null, null);
-        // (byte[] inputId, Optional<ZenBoxData> outputData, Signature25519 proof, long fee, byte version, KeyRotationProof keyRotationProof)
+        Enumeration.Value keyRotationProofType = KeyRotationProofType.Value(keyTypeEnumerationNumber);
+        KeyRotationProof keyRotationProof = new KeyRotationProof(keyRotationProofType, indexOfKey, newValueOfKey, signingKeySignature, masterKeySignature);
         KeyRotationTransaction transaction = new KeyRotationTransaction(from.getKey().id(), output, (Signature25519) secret.sign(messageToSign), fee, KEY_ROTATION_TRANSACTION_VERSION, keyRotationProof);
         transaction.transactionSemanticValidity();
 
