c821f8a0502889295237b2808e456cc2b61d79d1
sgonta <sgonta@horizenlabs.io>
2022-10-21 10:24:44 +0300

    Deleted merkle root method

--- sdk/src/main/scala/com/horizen/certificatesubmitter/keys/CertifiersKeys.scala
+++ sdk/src/main/scala/com/horizen/certificatesubmitter/keys/CertifiersKeys.scala
@@ -1,26 +1,13 @@
 package com.horizen.certificatesubmitter.keys
 
 import com.horizen.proposition.{SchnorrProposition, SchnorrPropositionSerializer}
-import com.horizen.utils.MerkleTree
-import scorex.crypto.hash.Sha256
 import scorex.util.serialization.{Reader, Writer}
 import sparkz.core.serialization.{BytesSerializable, SparkzSerializer}
-
-import scala.collection.JavaConverters.seqAsJavaListConverter
 
 case class CertifiersKeys(signingKeys: Vector[SchnorrProposition], masterKeys: Vector[SchnorrProposition]) extends BytesSerializable {
   override type M = CertifiersKeys
 
   override def serializer: SparkzSerializer[CertifiersKeys] = ActualKeysSerializer
-}
-
-object CertifiersKeys {
-  def getMerkleRootOfPublicKeys(actualKeys: CertifiersKeys): Array[Byte] = {
-    val hashes = (for (i <- actualKeys.signingKeys.indices) yield {
-      Sha256.hash(actualKeys.signingKeys(i).pubKeyBytes(), actualKeys.masterKeys(i).pubKeyBytes()).asInstanceOf[Array[Byte]]
-    }).toList.asJava
-    MerkleTree.createMerkleTree(hashes).rootHash()
-  }
 }
 
 object ActualKeysSerializer extends SparkzSerializer[CertifiersKeys] {
