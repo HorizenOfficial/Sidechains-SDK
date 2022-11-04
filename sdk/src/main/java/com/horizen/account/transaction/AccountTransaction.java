e3306ed74dbed4c93b9522721eeb7a10458157b0
Alberto Sala <alberto.sala@nttdata.com>
2022-11-04 11:46:50 +0100

    Removed useless abstract declaration and fix comment

--- sdk/src/main/java/com/horizen/account/transaction/AccountTransaction.java
+++ sdk/src/main/java/com/horizen/account/transaction/AccountTransaction.java
@@ -3,12 +3,10 @@ package com.horizen.account.transaction;
 import com.horizen.proof.Proof;
 import com.horizen.proposition.Proposition;
 import com.horizen.transaction.Transaction;
-import com.horizen.transaction.exception.TransactionSemanticValidityException;
 
 import java.math.BigInteger;
 
 public abstract class AccountTransaction<P extends Proposition, PR extends Proof<P>> extends Transaction {
-    public abstract void semanticValidity() throws TransactionSemanticValidityException;
 
     public abstract BigInteger getNonce();
 
@@ -29,14 +27,13 @@ public abstract class AccountTransaction<P extends Proposition, PR extends Proof
 
     /* Maximum fee per unit of gas a user is willing to pay for their transaction to be executed.
     * It must be greater or equal than the sum of the base fee and the priority fee ("tip").
-    * It is used together with max priority fee per gas*/
+    * It is used together with max priority fee per gas. */
     public abstract BigInteger getMaxFeePerGas();
 
-    /* Optional fee per unit of gas a user is willing to pay for their transaction to be executed, in addition to base
+    /* Fee per unit of gas a user is willing to pay for their transaction to be executed, in addition to base
     fee. It is the part of the gas fee paid directly to the block forger. In case the sum of base fee and priority fee
     is greater than max fee per gas, the user will pay just max fee per gas, thus the effective priority fee will be
-    max fee - base fee.
-   */
+    max fee - base fee. */
     public abstract BigInteger getMaxPriorityFeePerGas();
 
 }
