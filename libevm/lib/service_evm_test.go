00922337ee68436ff011b0ce3b150211801ca70a
Jan Christoph Bernack <jc.bernack@gmail.com>
2022-11-22 21:30:25 +0100

    add tests for EVM context opcodes

--- libevm/lib/service_evm_test.go
+++ libevm/lib/service_evm_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"libevm/test"
 	"math/big"
+	"reflect"
 	"testing"
 )
 
@@ -66,5 +67,96 @@ func TestEvmTrace(t *testing.T) {
 	}
 	if sstoreInstructions != 1 {
 		t.Fatalf("unexpected number of SSTORE instructions: expected %d, actual %d", 1, sstoreInstructions)
+	}
+}
+
+func TestEvmOpCodes(t *testing.T) {
+	var (
+		instance = New()
+		user     = common.HexToAddress("0x42")
+	)
+	dbHandle := instance.OpenMemoryDB()
+	_, stateHandle := instance.StateOpen(StateParams{
+		DatabaseParams: DatabaseParams{
+			dbHandle,
+		},
+		Root: test.EmptyHash,
+	})
+	_, statedb := instance.statedbs.Get(stateHandle)
+	_, resultDeploy := instance.EvmApply(EvmParams{
+		HandleParams: HandleParams{Handle: stateHandle},
+		From:         user,
+		To:           nil,
+		Input:        test.OpCodesContractDeploy(),
+		AvailableGas: 200000,
+	})
+	if resultDeploy.EvmError != "" {
+		t.Fatalf("vm error: %v", resultDeploy.EvmError)
+	}
+	deployedCode := statedb.GetCode(*resultDeploy.ContractAddress)
+	if common.Bytes2Hex(test.OpCodesContractRuntimeCode()) != common.Bytes2Hex(deployedCode) {
+		t.Fatalf("deployed code does not match %s", common.Bytes2Hex(deployedCode))
+	}
+
+	var (
+		gasPrice    = big.NewInt(586732)
+		chainID     = uint64(1337)
+		coinbase    = common.HexToAddress("0x09a1e4d0c6f6055287a6e1553a1d9cfe05767591")
+		gasLimit    = uint64(32123457)
+		blockNumber = big.NewInt(51231287)
+		time        = big.NewInt(1669144595)
+		baseFee     = big.NewInt(123872)
+		random      = common.HexToHash("0x0a5d85d0f0e021c04643e05e38f8f28029275683ee743910670154d78322b6eb")
+	)
+
+	// redefine this interface here, because it is not exported from GETH
+	type bytesBacked interface {
+		Bytes() []byte
+	}
+
+	checks := []struct {
+		name     string
+		expected bytesBacked
+	}{
+		{"GASPRICE", gasPrice},
+		{"CHAINID", new(big.Int).SetUint64(chainID)},
+		{"COINBASE", coinbase},
+		{"GASLIMIT", new(big.Int).SetUint64(gasLimit)},
+		{"BLOCKNUMBER", blockNumber},
+		{"TIME", time},
+		{"BASEFEE", baseFee},
+		{"RANDOM", random},
+	}
+
+	for _, check := range checks {
+		t.Run(check.name, func(t *testing.T) {
+			// call function and verify result value
+			err, result := instance.EvmApply(EvmParams{
+				HandleParams: HandleParams{Handle: stateHandle},
+				From:         user,
+				To:           resultDeploy.ContractAddress,
+				Input:        test.OpCodesContractCall(check.name),
+				AvailableGas: 200000,
+				GasPrice:     (*hexutil.Big)(gasPrice),
+				Context: EvmContext{
+					ChainID:     hexutil.Uint64(chainID),
+					Coinbase:    coinbase,
+					GasLimit:    hexutil.Uint64(gasLimit),
+					BlockNumber: (*hexutil.Big)(blockNumber),
+					Time:        (*hexutil.Big)(time),
+					BaseFee:     (*hexutil.Big)(baseFee),
+					Random:      &random,
+				},
+			})
+			if err != nil {
+				t.Fatalf("error: %v", result.EvmError)
+			}
+			if result.EvmError != "" {
+				t.Fatalf("vm error: %v", result.EvmError)
+			}
+			if expected := common.LeftPadBytes(check.expected.Bytes(), 32); !reflect.DeepEqual(expected, result.ReturnData) {
+				t.Fatalf("test failed for %v:\n%v expected\n%v actual", check.name, expected, result.ReturnData)
+			}
+		})
 	}
 }
---
+++ libevm/test/OpCodes.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0 <0.9.0;
+
+/**
+ * @title OpCodes
+ * @dev uses specific opcodes for testing
+ */
+contract OpCodes {
+
+    function getGasPrice() external view returns (uint256) {
+        return tx.gasprice;
+    }
+
+    function getChainID() external view returns (uint256) {
+        return block.chainid;
+    }
+
+    function getCoinbase() external view returns (address) {
+        return block.coinbase;
+    }
+
+    function getGasLimit() external view returns (uint256) {
+        return block.gaslimit;
+    }
+
+    function getBlockNumber() external view returns (uint256) {
+        return block.number;
+    }
+
+    function getTime() external view returns (uint256) {
+        return block.timestamp;
+    }
+
+    function getBaseFee() external view returns (uint256) {
+        return block.basefee;
+    }
+
+    function getRandom() external view returns (uint256) {
+        // TODO: use block.prevrandao as soons as Solidity has this implemented
+        // note: the emitted opcode remains the same, so there is no functional difference - it's just a cosmetic rename
+        return block.difficulty;
+    }
+}
