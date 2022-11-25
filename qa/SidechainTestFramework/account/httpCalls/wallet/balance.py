6060dcecba7d8b4e56eb483226d7bd3c80d6988a
akrckn <alexander@rocknitive.com>
2022-11-25 08:36:03 +0100

    update default value and reformat balance helper function

--- qa/SidechainTestFramework/account/httpCalls/wallet/balance.py
+++ qa/SidechainTestFramework/account/httpCalls/wallet/balance.py
@@ -1,17 +1,15 @@
 import json
 
-#executes a wallet/balance call on an account sidechain
-def http_wallet_balance(sidechainNode, evm_address, api_key = 'Horizen'):
-      j = {"address": str(evm_address)}
-      balance_request = json.dumps(j)
 
-      # balance is in wei
-      if (api_key != None):
-            response = sidechainNode.wallet_getBalance(balance_request, api_key)
-      else:
-            response = sidechainNode.wallet_getBalance(balance_request)
+# executes a wallet/balance call on an account sidechain
+def http_wallet_balance(sidechainNode, evm_address, api_key=None):
+    j = {"address": str(evm_address)}
+    balance_request = json.dumps(j)
 
-      return response["result"]["balance"]
+    # balance is in wei
+    if api_key is not None:
+        response = sidechainNode.wallet_getBalance(balance_request, api_key)
+    else:
+        response = sidechainNode.wallet_getBalance(balance_request)
 
-
-
+    return response["result"]["balance"]
