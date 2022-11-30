9e7d431a4a740ec72caaf8f2de5916fb68467619
GitMark0 <marko.ivankovic650@gmail.com>
2022-11-30 12:13:43 +0100

    remove declared methods

--- sdk/src/main/java/com/horizen/account/node/NodeAccountMemoryPool.java
+++ sdk/src/main/java/com/horizen/account/node/NodeAccountMemoryPool.java
@@ -5,19 +5,5 @@ import com.horizen.node.NodeMemoryPoolBase;
 import com.horizen.proof.Proof;
 import com.horizen.proposition.Proposition;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
-
-
 public interface NodeAccountMemoryPool extends NodeMemoryPoolBase<AccountTransaction<Proposition, Proof<Proposition>>> {
-    List<AccountTransaction<Proposition, Proof<Proposition>>> getTransactions();
-
-    List<AccountTransaction<Proposition, Proof<Proposition>>> getTransactions(
-            Comparator<AccountTransaction<Proposition, Proof<Proposition>>> c,
-            int limit);
-
-    int getSize();
-
-    Optional<AccountTransaction<Proposition, Proof<Proposition>>> getTransactionById(String transactionId);
 }
