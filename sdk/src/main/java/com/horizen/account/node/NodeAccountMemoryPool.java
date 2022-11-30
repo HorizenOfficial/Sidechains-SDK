f203f013d58d54b8cdf8864fd3f3be2e460b5cbe
GitMark0 <marko.ivankovic650@gmail.com>
2022-11-30 12:12:38 +0100

    remove unused imports

--- sdk/src/main/java/com/horizen/account/node/NodeAccountMemoryPool.java
+++ sdk/src/main/java/com/horizen/account/node/NodeAccountMemoryPool.java
@@ -1,11 +1,9 @@
 package com.horizen.account.node;
 
 import com.horizen.account.transaction.AccountTransaction;
-import com.horizen.box.Box;
 import com.horizen.node.NodeMemoryPoolBase;
 import com.horizen.proof.Proof;
 import com.horizen.proposition.Proposition;
-import com.horizen.transaction.BoxTransaction;
 
 import java.util.Comparator;
 import java.util.List;
