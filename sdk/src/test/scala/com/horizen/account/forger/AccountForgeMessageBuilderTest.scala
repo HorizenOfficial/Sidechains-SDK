e87e733db3694f318765a3d724c0ccc63b664b7b
GitMark0 <marko.ivankovic650@gmail.com>
2022-12-17 15:44:04 +0100

    add missing test for createNewBlock WIP

--- sdk/src/test/scala/com/horizen/account/forger/AccountForgeMessageBuilderTest.scala
+++ sdk/src/test/scala/com/horizen/account/forger/AccountForgeMessageBuilderTest.scala
@@ -1,16 +1,31 @@
 package com.horizen.account.forger
 
-import com.horizen.SidechainTypes
+import com.horizen.account.block.AccountBlockHeader
+import com.horizen.{SidechainTypes, SidechainWallet}
 import com.horizen.account.fixtures.EthereumTransactionFixture
 import com.horizen.account.proof.SignatureSecp256k1
 import com.horizen.account.mempool.TransactionsByPriceAndNonceIter
+import com.horizen.account.secret.PrivateKeySecp256k1
 import com.horizen.account.state._
 import com.horizen.account.transaction.EthereumTransaction
+import com.horizen.block.{MainchainBlockReferenceData, MainchainHeader, Ommer}
+import com.horizen.chain.MainchainHeaderHash
+import com.horizen.consensus.ForgingStakeInfo
+import com.horizen.proof.{Signature25519, VrfProof}
+import com.horizen.secret.PrivateKey25519
+import com.horizen.transaction.TransactionSerializer
+import com.horizen.utils.{DynamicTypedSerializer, MerklePath}
 import org.junit.Assert.{assertArrayEquals, assertEquals, assertTrue}
 import org.junit.Test
 import org.mockito.{ArgumentMatchers, Mockito}
+import org.scalatest.Assertions.assertThrows
 import org.scalatestplus.mockito.MockitoSugar
+import scorex.util.ModifierId
+import sparkz.core.block.Block.BlockId
+import sparkz.core.transaction.state.Secret
+
 import java.math.BigInteger
+import java.util
 
 class AccountForgeMessageBuilderTest
     extends MockitoSugar
@@ -172,6 +187,47 @@ class AccountForgeMessageBuilderTest
       assertEquals(1, appliedTxs.size)
       assertEquals(validTx.id(), appliedTxs.head.id)
     }
+  }
+
+  @Test
+  def testCreateNewBlockFailingIfAddressSizeIsZero(): Unit = {
+    val nodeView = mock[forger.View]
+    Mockito.when(nodeView.vault.secretsOfType(classOf[PrivateKeySecp256k1]))
+      .thenReturn(List.empty[Secret])
+
+    val branchPointInfo = mock[forger.BranchPointInfo]
+    val parentId = mock[BlockId]
+    val mainchainBlockReferencesData = Seq(mock[MainchainBlockReferenceData])
+    val sidechainTransactions = mock[Iterable[SidechainTypes#SCAT]]
+    val mainchainHeaders = Seq(mock[MainchainHeader])
+    val ommers = Seq(mock[Ommer[AccountBlockHeader]])
+    val ownerPrivateKey = mock[PrivateKey25519]
+    val forgingStakeInfo = mock[ForgingStakeInfo]
+    val vrfProof = mock[VrfProof]
+    val forgingStakeInfoMerklePath = mock[MerklePath]
+    val companion = mock[DynamicTypedSerializer[SidechainTypes#SCAT, TransactionSerializer[SidechainTypes#SCAT]]]
+    val inputBlockSize = 0
+    val signatureOption = Some(mock[Signature25519])
+
+    val forger = new AccountForgeMessageBuilder(null, null, null, false)
+
+    assertThrows[IllegalArgumentException](classOf[IllegalArgumentException], forger.createNewBlock(
+      nodeView,
+      branchPointInfo,
+      false,
+      parentId,
+      0L,
+      mainchainBlockReferencesData,
+      sidechainTransactions,
+      mainchainHeaders,
+      ommers,
+      ownerPrivateKey,
+      forgingStakeInfo,
+      vrfProof,
+      forgingStakeInfoMerklePath,
+      companion,
+      inputBlockSize,
+      signatureOption))
   }
 
   private def setupMockMessageProcessor = {
