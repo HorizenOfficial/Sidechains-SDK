5fff880416e99802773596142a637cec298e26f7
davehorizen <davidebanfi@horizenlabs.io>
2022-12-20 17:49:34 +0100

    db/EVM-616 - debug methods request structure update

--- evm/src/main/java/com/horizen/evm/interop/TraceOptions.java
+++ evm/src/main/java/com/horizen/evm/interop/TraceOptions.java
@@ -3,16 +3,50 @@ package com.horizen.evm.interop;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TraceOptions {
-    public final boolean enableMemory;
-    public final boolean disableStack;
-    public final boolean disableStorage;
-    public final boolean enableReturnData;
+public class TraceParams {
+    private final boolean enableMemory;
+    private final boolean disableStack;
+    private final boolean disableStorage;
+    private final boolean enableReturnData;
+    private final String tracer;
+    private final String timeout;
+    private final TracerConfig tracerConfig;
 
-    public TraceOptions() {
-        enableMemory = true;
+    public TraceParams() {
+        enableMemory = false;
         disableStack = false;
         disableStorage = false;
-        enableReturnData = true;
+        enableReturnData = false;
+        tracer = null;
+        timeout = null;
+        tracerConfig = null;
+    }
+
+    public boolean isEnableMemory() {
+        return enableMemory;
+    }
+
+    public boolean isDisableStack() {
+        return disableStack;
+    }
+
+    public boolean isDisableStorage() {
+        return disableStorage;
+    }
+
+    public boolean isEnableReturnData() {
+        return enableReturnData;
+    }
+
+    public String getTracer() {
+        return tracer;
+    }
+
+    public String getTimeout() {
+        return timeout;
+    }
+
+    public TracerConfig getTracerConfig() {
+        return tracerConfig;
     }
 }
