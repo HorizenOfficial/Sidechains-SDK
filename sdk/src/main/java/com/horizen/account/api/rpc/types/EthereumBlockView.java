7ff2c6a37ec6d0f25e570f674105e6ac17a36d8a
davehorizen <davidebanfi@horizenlabs.io>
2022-12-22 09:44:00 +0100

    db/EVM-622 - difficulty and total difficulty in block

--- sdk/src/main/java/com/horizen/account/api/rpc/types/EthereumBlockView.java
+++ sdk/src/main/java/com/horizen/account/api/rpc/types/EthereumBlockView.java
@@ -58,7 +58,8 @@ public class EthereumBlockView {
         this.gasUsed = Numeric.toHexStringWithPrefix(BigInteger.valueOf(blockHeader.gasUsed()));
         this.timestamp = Numeric.prependHexPrefix(Long.toHexString(block.timestamp()));
         this.baseFeePerGas = Numeric.toHexStringWithPrefix(blockHeader.baseFee());
-        this.difficulty = Numeric.encodeQuantity(BigInteger.valueOf(blockNumber));
+        this.difficulty = "0x0";
+        this.totalDifficulty = "0x0";
 
         if (!hydratedTx) {
             var transactions = scala.collection.JavaConverters.seqAsJavaList(block.transactions());
