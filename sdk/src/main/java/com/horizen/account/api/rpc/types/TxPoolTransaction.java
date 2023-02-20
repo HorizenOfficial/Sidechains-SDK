7750bd28b1b492fb179387964f42c94305ff7de4
Jan Christoph Bernack <jc.bernack@gmail.com>
2023-02-20 16:04:55 +0100

    cleanup

--- sdk/src/main/java/com/horizen/account/api/rpc/types/TxPoolTransaction.java
+++ sdk/src/main/java/com/horizen/account/api/rpc/types/TxPoolTransaction.java
@@ -12,30 +12,28 @@ import java.math.BigInteger;
 @JsonView(Views.Default.class)
 @JsonInclude()
 public class TxPoolTransaction {
-    // fixed default value
+    // fixed default values
     public final Hash blockHash = Hash.ZERO;
-    // always null
     public final BigInteger blockNumber = null;
-    // always null
     public final BigInteger transactionIndex = null;
 
     public final Hash hash;
+    public final BigInteger nonce;
     public final Address from;
     public final Address to;
-    public final BigInteger nonce;
+    public final BigInteger value;
+    public final byte[] input;
     public final BigInteger gas;
     public final BigInteger gasPrice;
-    public final byte[] input;
-    public final BigInteger value;
 
     public TxPoolTransaction(EthereumTransaction tx) {
         this.hash = new Hash("0x" + tx.id());
+        this.nonce = tx.getNonce();
         this.from = tx.getFromAddress();
         this.to = tx.getToAddress();
-        this.nonce = tx.getNonce();
+        this.value = tx.getValue();
+        this.input = tx.getData();
         this.gas = tx.getGasLimit();
         this.gasPrice = tx.getGasPrice();
-        this.input = tx.getData();
-        this.value = tx.getValue();
     }
 }
