870b2a26ac7d044d0577e15c4cbb4fac1cb10383
MarcoOl94 <marco.olivero94@libero.it>
2023-02-22 11:41:05 +0100

    Added python test for the account websocket

---
+++ qa/SidechainTestFramework/account_websocket_client.py
@@ -0,0 +1,35 @@
+import websocket
+from websocket import create_connection
+from test_framework.util import assert_equal, assert_true
+import time
+import json
+
+class AccountWebsocketClient():
+    # Websocket account request
+    SUBSCRIBE_REQUEST = "eth_subscribe"
+    UNSUBSCRIBE_REQUEST = "eth_unsubscribe"
+
+    # Websocket account response
+    SUBSCRIBE_RESPONSE = "eth_subscription"
+    UNSUBSCRIBE_RESPONSE = "eth_unsubscription"
+
+
+    # Websocket account subscriptions
+    NEW_HEADS_SUBSCRIPTION = "newHeads"
+    NEW_PENDING_TRANSACTIONS_SUBSCRIPTION = "newPendingTransactions"
+    LOGS_SUBSCRIPTION = "logs"
+
+    def create_connection(self, url):
+        return create_connection(url)
+
+    def sendMessage(self, ws, id, request, params):
+        wsRequest = {"jsonrpc": "2.0","id": id, "method": request, "params": params}
+        ws.send(json.dumps(wsRequest))
+        timeout = 500
+        for x in range(1, timeout):
+            results = ws.recv()
+            if len(results)==0:
+                time.sleep(1)
+            else:
+                break
+        return results
