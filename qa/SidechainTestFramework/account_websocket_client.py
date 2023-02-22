048c4006f04dcde6fbd1e8dd724a7fea102ce298
MarcoOl94 <marco.olivero94@libero.it>
2023-02-22 11:41:05 +0100

    Added python test for the account websocket

--- qa/SidechainTestFramework/account_websocket_client.py
+++ qa/SidechainTestFramework/account_websocket_client.py
@@ -11,7 +11,6 @@ class AccountWebsocketClient():
     SUBSCRIBE_RESPONSE = "eth_subscription"
     UNSUBSCRIBE_RESPONSE = "eth_unsubscription"
 
-
     # Websocket account subscriptions
     NEW_HEADS_SUBSCRIPTION = "newHeads"
     NEW_PENDING_TRANSACTIONS_SUBSCRIPTION = "newPendingTransactions"
@@ -22,12 +21,12 @@ class AccountWebsocketClient():
         return create_connection(url)
 
     def sendMessage(self, ws, id, request, params):
-        wsRequest = {"jsonrpc": "2.0","id": id, "method": request, "params": params}
+        wsRequest = {"jsonrpc": "2.0", "id": id, "method": request, "params": params}
         ws.send(json.dumps(wsRequest))
         timeout = 500
         for x in range(1, timeout):
             results = ws.recv()
-            if len(results)==0:
+            if len(results) == 0:
                 time.sleep(1)
             else:
                 break
