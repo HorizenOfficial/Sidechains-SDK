b90db398af7e3555d849f1c6d784f17b42d0f36a
MarcoOl94 <marco.olivero94@libero.it>
2023-02-24 11:39:18 +0100

    Changed the way on how the account websocket handles chain reorganization

--- sdk/src/main/scala/com/horizen/account/websocket/WebSocketAccountChannel.scala
+++ sdk/src/main/scala/com/horizen/account/websocket/WebSocketAccountChannel.scala
@@ -2,13 +2,16 @@ package com.horizen.account.websocket
 
 import com.fasterxml.jackson.databind.node.ObjectNode
 import com.horizen.account.block.AccountBlock
+import com.horizen.account.receipt.EthereumReceipt
 import com.horizen.account.websocket.data.SubscriptionWithFilter
 
 trait WebSocketAccountChannel {
 
   def getWalletKeys: Set[String]
 
-  def getTransactionLogData(txHash: String, subscriptionWithFilter: SubscriptionWithFilter): Array[ObjectNode]
+  def getTransactionReceipt(txHash: String): Option[EthereumReceipt]
+
+  def createWsLogEventFromEthereumReceipt(txReceipt: EthereumReceipt, subscriptionWithFilter: SubscriptionWithFilter): Array[ObjectNode]
 
   def accountBlockToWebsocketJson(block: AccountBlock): ObjectNode
 
