a7bf6fdf6053bbf358a8d8bda9194b917f01c164
Alberto Sala <alberto.sala@nttdata.com>
2023-03-03 16:54:26 +0100

    Code cleanup

--- sdk/src/main/scala/io/horizen/account/abi/MsgProcessorInputDecoder.scala
+++ sdk/src/main/scala/io/horizen/account/abi/MsgProcessorInputDecoder.scala
@@ -6,7 +6,7 @@ import scala.util.{Failure, Success, Try}
 
 trait MsgProcessorInputDecoder[A] extends ABIDecoder[A] {
 
-  abstract override def decode(abiEncodedData: Array[Byte]): A = {
+  override def decode(abiEncodedData: Array[Byte]): A = {
     Try {
       // We must trap any exception arising in case of wrong data that can not be decoded according to the types
       // and return the execution reverted exception
