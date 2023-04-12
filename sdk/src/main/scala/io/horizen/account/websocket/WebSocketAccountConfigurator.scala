52059ddef3fc2a794d9351317d35d09cc71d813e
MarcoOl94 <marco.olivero94@libero.it>
2023-04-12 11:42:44 +0200

    Changes after code review

--- sdk/src/main/scala/io/horizen/account/websocket/WebSocketAccountConfigurator.scala
+++ sdk/src/main/scala/io/horizen/account/websocket/WebSocketAccountConfigurator.scala
@@ -5,20 +5,20 @@ import jakarta.websocket.server.{HandshakeRequest, ServerEndpointConfig}
 import org.glassfish.tyrus.core.RequestContext
 import sparkz.util.SparkzLogging
 
+import java.net.InetAddress
 
 class WebSocketAccountConfigurator() extends ServerEndpointConfig.Configurator with SparkzLogging {
 
   override def modifyHandshake(config: ServerEndpointConfig, request: HandshakeRequest, response: HandshakeResponse): Unit = {
-    val remoteAddress = request.asInstanceOf[RequestContext].getRemoteAddr
-    if (!WebSocketAccountConfigurator.allowedIPs.contains(remoteAddress)) {
+    val remoteAddress = InetAddress.getByName(request.asInstanceOf[RequestContext].getRemoteAddr)
+    if (!remoteAddress.isAnyLocalAddress && !remoteAddress.isLoopbackAddress && !remoteAddress.isSiteLocalAddress && !WebSocketAccountConfigurator.allowedIPs.contains(remoteAddress.getHostAddress))
       throw new SecurityException(s"IP ${remoteAddress} is not allowed to connect to the WebSocket")
-    }
   }
 
 }
 
 object WebSocketAccountConfigurator {
-  var allowedIPs = Seq("127.0.0.1")
+  var allowedIPs: Seq[String] = Seq()
   def apply(allowedIPs: Seq[String]): Unit = {
     this.allowedIPs = allowedIPs
   }
