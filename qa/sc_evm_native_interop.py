e8866a48f14b7898979a950ea2b0bef95acdddf0
Jan Christoph Bernack <jc.bernack@gmail.com>
2023-05-02 21:14:03 +0200

    add test for tracing with native interop

--- qa/sc_evm_native_interop.py
+++ qa/sc_evm_native_interop.py
@@ -5,7 +5,7 @@ from SidechainTestFramework.account.ac_chain_setup import AccountChainSetup
 from SidechainTestFramework.account.ac_use_smart_contract import SmartContract
 from SidechainTestFramework.account.ac_utils import deploy_smart_contract
 from SidechainTestFramework.account.utils import FORGER_STAKE_SMART_CONTRACT_ADDRESS
-from test_framework.util import assert_equal
+from test_framework.util import assert_equal, assert_true, assert_false
 
 """
 Check an EVM Contract calling a native contract.
@@ -61,7 +61,52 @@ class SCEvmNativeInterop(AccountChainSetup):
 
         # Verify identical results
         assert_equal(expected_value, actual_value, "results do not match")
-        logging.info("")
+
+        # Verify tracing gives reasonable result for the call from EVM contract to native contract
+        trace_response = node.rpc_debug_traceCall(
+            {
+                "to": contract_address,
+                "input": "0xd9908c86"
+            }, "latest", {
+                "tracer": "callTracer"
+            }
+        )
+        assert_false("error" in trace_response)
+        assert_true("result" in trace_response)
+        trace_result = trace_response["result"]
+        logging.info("trace result: {}".format(trace_result))
+
+        # Expected output
+        # {
+        #   "type": "CALL",
+        #   "from": "0x0000000000000000000000000000000000000000",
+        #   "to": "0x840463d17b8c7833883eaa47d23b2646f7fd1fd9",
+        #   "value": "0x0",
+        #   "gas": "0x2fa9e38",
+        #   "gasUsed": "0x6b9e",
+        #   "input": "0xd9908c86",
+        #   "output": "0x000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000013941b55d40a2cac0485248eca396e72237d9ca08e07f686989ffff37f1d320960000000000000000000000000000000000000000000000056bc75e2d63100000000000000000000000000000375ea7214743b3ad892beed86999a1f5a6794ad76e3bda4dfddf67e293362514c36142f70862dab22cd3609face526aec9b1c809dbfb30791dbc1b1d0140fea9c49cd2ca0d6aade8139ee919cc4795e11ae9c1040000000000000000000000000000000000000000000000000000000000000000",
+        #   "calls": [
+        #     {
+        #       "type": "STATICCALL",
+        #       "from": "0x840463d17b8c7833883eaa47d23b2646f7fd1fd9",
+        #       "to": "0x0000000000000000000022222222222222222222",
+        #       "gas": "0x186a0",
+        #       "gasUsed": "0x49d4",
+        #       "input": "0xf6ad3c23",
+        #       "output": "0x000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000013941b55d40a2cac0485248eca396e72237d9ca08e07f686989ffff37f1d320960000000000000000000000000000000000000000000000056bc75e2d63100000000000000000000000000000375ea7214743b3ad892beed86999a1f5a6794ad76e3bda4dfddf67e293362514c36142f70862dab22cd3609face526aec9b1c809dbfb30791dbc1b1d0140fea9c49cd2ca0d6aade8139ee919cc4795e11ae9c1040000000000000000000000000000000000000000000000000000000000000000"
+        #     }
+        #   ]
+        # }
+        assert_equal(contract_address.lower(), trace_result["to"].lower())
+        native_call = trace_result["calls"][0]
+        assert_equal("STATICCALL", native_call["type"])
+        assert_equal(contract_address.lower(), native_call["from"].lower())
+        assert_equal("0x" + FORGER_STAKE_SMART_CONTRACT_ADDRESS, native_call["to"])
+        assert_true(int(native_call["gas"], 16) > 0)
+        assert_true(int(native_call["gasUsed"], 16) > 0)
+        assert_equal("0xf6ad3c23", native_call["input"])
+        assert_true(len(native_call["output"]) > 512)
 
 
 if __name__ == "__main__":
