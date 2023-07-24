833e7233d083544900bc4872dc6dd354cf931b2e
Yuri Sibirski <yuri@horizenlabs.io>
2023-07-24 14:05:24 -0400

    CI/CD GH releases + rework

--- ci/script.sh
+++ ci/script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # shellcheck disable=SC2046
-
 set -euo pipefail
 
 command -v docker &> /dev/null && have_docker="true" || have_docker="false"
@@ -10,37 +9,44 @@ workdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
 DOCKER_ORG="${DOCKER_ORG:-zencash}"
 IMAGE_NAME="${IMAGE_NAME:-sc-ci-base}"
 IMAGE_TAG="${IMAGE_TAG:-bionic_jdk-11_latest}"
+TESTS="${TESTS:-}"
+IS_A_RELEASE="${IS_A_RELEASE:-false}"
 image="${DOCKER_ORG}/${IMAGE_NAME}:${IMAGE_TAG}"
-export CONTAINER_PUBLISH="${CONTAINER_PUBLISH:-false}"
 
-if [ "${1}" = "tests" ]; then
-  # run tests in docker or natively
-  if [ -n "${TESTS:-}" ]; then
-    if [ "$have_docker" = "true" ]; then
-      if [ -n "${DOCKER_READONLY_USERNAME:-}" ] && [ -n "${DOCKER_READONLY_PASSWORD:-}" ]; then
-        echo "$DOCKER_READONLY_PASSWORD" | docker login -u "$DOCKER_READONLY_USERNAME" --password-stdin
-      fi
-      docker run --rm -t -v "$workdir":/build -w /build \
-        --entrypoint /build/ci/docker/entrypoint.sh \
-        -v "${HOME}"/key.asc:/key.asc \
-        -e CONTAINER_PUBLISH \
-        -e TESTS \
-        -e LOCAL_USER_ID="$(id -u)" \
-        -e LOCAL_GRP_ID="$(id -g)"\
-        "${image}" ci/run_tests.sh
-    else
-      cd "${workdir:-.}" && ci/run_tests.sh
-    fi
-  fi
-elif [ "${1}" = "release" ]; then
+
+# Run tests
+if [ -n "${TESTS:-}" ]; then
+  echo "" && echo "=== Running tests ===" && echo ""
+
   if [ "$have_docker" = "true" ]; then
-    if [ -n "${DOCKER_READONLY_USERNAME:-}" ] && [ -n "${DOCKER_READONLY_PASSWORD:-}" ]; then
-      echo "$DOCKER_READONLY_PASSWORD" | docker login -u "$DOCKER_READONLY_USERNAME" --password-stdin
+    if [ -n "${DOCKER_USERNAME:-}" ] && [ -n "${DOCKER_PASSWORD:-}" ]; then
+      echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     fi
     docker run --rm -t -v "$workdir":/build -w /build \
       --entrypoint /build/ci/docker/entrypoint.sh \
       -v "${HOME}"/key.asc:/key.asc \
-      -e CONTAINER_PUBLISH \
+      -e IS_A_RELEASE \
+      -e TESTS \
+      -e LOCAL_USER_ID="$(id -u)" \
+      -e LOCAL_GRP_ID="$(id -g)"\
+      "${image}" ci/run_tests.sh
+  else
+    cd "${workdir:-.}" && ci/run_tests.sh
+  fi
+fi
+
+# Publish java project stage
+if [ "${IS_A_RELEASE}" = "true" ]; then
+  echo "" && echo "=== Publishing java project on public repository ===" && echo ""
+
+  if [ "$have_docker" = "true" ]; then
+    if [ -n "${DOCKER_USERNAME:-}" ] && [ -n "${DOCKER_PASSWORD:-}" ]; then
+      echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    fi
+    docker run --rm -t -v "$workdir":/build -w /build \
+      --entrypoint /build/ci/docker/entrypoint.sh \
+      -v "${HOME}"/key.asc:/key.asc \
+      -e IS_A_RELEASE \
       -e TRAVIS_TAG \
       -e LOCAL_USER_ID="$(id -u)" \
       -e LOCAL_GRP_ID="$(id -g)"\
