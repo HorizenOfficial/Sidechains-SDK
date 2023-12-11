fb37978ff0c77768cbfa2b128e640de1e63b4417
Ivan Skrypnyk <vindoraga@gmail.com>
2023-12-11 10:51:08 +0200

    SDK-1627 - specific signature V field json serialization for transaction type 2

---
+++ sdk/src/main/java/io/horizen/json/serializer/SignatureVFieldSerializer.java
@@ -0,0 +1,22 @@
+package io.horizen.json.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import io.horizen.account.transaction.EthereumTransaction;
+import org.web3j.crypto.Sign;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+
+public class SignatureVFieldSerializer extends JsonSerializer<BigInteger> {
+    @Override
+    public void serialize(BigInteger val, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        Object pojo = jsonGenerator.getOutputContext().getParent().getCurrentValue();
+        if (pojo instanceof EthereumTransaction && ((EthereumTransaction) pojo).isLegacy())
+            jsonGenerator.writeString(val.toString(16));
+        else
+            jsonGenerator.writeString(val.subtract(BigInteger.valueOf(Sign.LOWER_REAL_V)).toString(16));
+    }
+}
