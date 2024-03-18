b1b2a2c2ba21a05ac91ab099db4cce0f240dbb9b
Ivan Skrypnyk <vindoraga@gmail.com>
2024-03-18 11:16:53 +0200

    SDK-1331 - web3_clientVersion should return EON version

--- sdk/src/main/scala/io/horizen/account/api/rpc/service/RpcUtils.scala
+++ sdk/src/main/scala/io/horizen/account/api/rpc/service/RpcUtils.scala
@@ -4,16 +4,12 @@ import scala.util.Try
 
 object RpcUtils {
 
-  def getClientVersion: String = {
+  def getClientVersion(appVersion: String): String = {
     val default = "dev"
     val architecture = Try(System.getProperty("os.arch")).getOrElse(default)
     val javaVersion = Try(System.getProperty("java.specification.version")).getOrElse(default)
     val sdkPackage = this.getClass.getPackage
-    val sdkTitle = sdkPackage.getImplementationTitle match {
-      case null => default
-      case title => Try(title.split(":")(1)).getOrElse(title)
-    }
     val sdkVersion = sdkPackage.getImplementationVersion
-    s"$sdkTitle/$sdkVersion/$architecture/jdk$javaVersion"
+    s"$appVersion/$sdkVersion/$architecture/jdk$javaVersion"
   }
 }
