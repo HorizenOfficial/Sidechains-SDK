78d056b2a55511d8f3989dedbcf3a7b6a5c91788
Ivan Skrypnyk <vindoraga@gmail.com>
2024-03-20 12:38:05 +0200

    review fixes

--- sdk/src/main/scala/io/horizen/account/api/rpc/service/RpcUtils.scala
+++ sdk/src/main/scala/io/horizen/account/api/rpc/service/RpcUtils.scala
@@ -6,10 +6,11 @@ object RpcUtils {
 
   def getClientVersion(appVersion: String): String = {
     val default = "dev"
+    val version = if (appVersion.isBlank) default else appVersion
     val architecture = Try(System.getProperty("os.arch")).getOrElse(default)
     val javaVersion = Try(System.getProperty("java.specification.version")).getOrElse(default)
     val sdkPackage = this.getClass.getPackage
     val sdkVersion = sdkPackage.getImplementationVersion
-    s"$appVersion/$sdkVersion/$architecture/jdk$javaVersion"
+    s"$version/$sdkVersion/$architecture/jdk$javaVersion"
   }
 }
